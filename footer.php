fc73cf21dff59080868382d673a7f2b28841b7a1
Chano Vera <github@chanovera.com>
2023-10-19 04:10:02 -0600

    agregada la estructura base del footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<?php
+        echo '<footer id="main-footer" class="container">
+            <section class="section">
+                
+            </section>
+        </footer>';
+        wp_footer();
+    echo '</body>
+</html>';
