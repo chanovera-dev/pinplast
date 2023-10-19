6cb9a5a906db6a7246f025f3626b4055932d103c
Chano Vera <github@chanovera.com>
2023-10-19 11:45:54 -0600

    pruebas para discriminación de el tamaño de pantalla

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
   var width = window.innerWidth;
-  var responsiveContent = document.getElementById('responsive-header');
+  var responsiveContent = document.getElementById('responsive-content');
+
   if (width < 768) {
       // Mostrar el contenido para pantallas pequeñas
-      responsiveContent.textContent = 'Pantalla pequeña';
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              responsiveContent.innerHTML = this.responseText;
+          }
+      };
+      xhttp.open("GET", "/parts/header/little-screen.php", true);
+      xhttp.send();
   } else {
       // Mostrar el contenido para pantallas grandes
-      responsiveContent.textContent = 'Pantalla grande';
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              responsiveContent.innerHTML = this.responseText;
+          }
+      };
+      xhttp.open("GET", "/parts/header/big-screen.php", true);
+      xhttp.send();
   }
 });
