344a28de3c46234a38c3232cadbad78f4e38f2d8
Chano Vera <github@chanovera.com>
2023-10-20 10:58:28 -0600

    ajustes al header mobile

--- parts/header/little-screen.php
+++ parts/header/little-screen.php
@@ -2,3 +2,4 @@
 include(TEMPLATEPATH . '/parts/header/menu-mobile.php');
 include(TEMPLATEPATH . '/parts/header/menu-button.php');
 include(TEMPLATEPATH . '/parts/header/brand.php');
+get_search_form();
