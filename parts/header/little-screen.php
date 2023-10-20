1c65f63d4b5a25b525ac227f51d6ae5a62522269
Chano Vera <github@chanovera.com>
2023-10-19 21:53:04 -0600

    ajustes al cajón de búsqueda

--- parts/header/little-screen.php
+++ parts/header/little-screen.php
@@ -2,4 +2,4 @@
 include(TEMPLATEPATH . '/parts/header/menu-mobile.php');
 include(TEMPLATEPATH . '/parts/header/menu-button.php');
 include(TEMPLATEPATH . '/parts/header/brand.php');
-get_search_form();
+include(TEMPLATEPATH . '/searchform.php');
