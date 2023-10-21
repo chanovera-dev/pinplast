3ff91e352ac4ad42c3f315ed1bd8efa8edcb5241
Chano Vera <github@chanovera.com>
2023-10-20 23:32:40 -0600

    ajustes al footer

--- functions/components.php
+++ functions/components.php
@@ -33,7 +33,8 @@ add_action( 'wp_enqueue_scripts', 'blog_styles' );
 function frontpage_styles() {
     if ( is_front_page() or is_page_template('front-page.php') ) {
         wp_dequeue_style( 'wp-block-library' );
-        wp_enqueue_style( 'hero-styles', get_template_directory_uri() . '/assets/css/frontpage/hero.css' ); 
+        wp_enqueue_style( 'hero-styles', get_template_directory_uri() . '/assets/css/frontpage/hero.css' );
+        wp_enqueue_style( 'widgets-styles', get_template_directory_uri() . '/assets/css/widgets.css' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'frontpage_styles' );
