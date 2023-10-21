6565a2dfdda06e793dc852056b1577b2b49d1cd8
Chano Vera <github@chanovera.com>
2023-10-20 23:55:52 -0600

    ajustes al footer

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <?php
         echo '<footer id="main-footer" class="container">
             <section class="section">
-                <div>
+                <aside>
                     <h2>'._('Contacto', 'pinplast').'</h2>
                     <p>'._('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer in feugiat lorem. Pellentque ac placerat tellus.', 'pinplast').'</p>';
                     include(TEMPLATEPATH . '/parts/widgets/address.php');
-                echo '</div>
+                echo '</aside>
                 <div></div>
                 <div></div>
                 <div></div>
