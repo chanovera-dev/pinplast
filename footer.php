acf1ae4af12eb96c60d588953a4b3d0842debefa
Chano Vera <github@chanovera.com>
2023-10-22 12:31:30 -0600

    ajustes en el footer

--- footer.php
+++ footer.php
@@ -33,32 +33,33 @@
                             ) 
                         ); 
                     echo '</div>';
-                echo '</div>';
-                $menu_id = get_nav_menu_locations()['social'];
-                $menu = wp_get_nav_menu_object($menu_id);
-                $items = wp_get_nav_menu_items($menu_id);
-                if (!empty($items)) {
-                    echo '<div class="newsletter">';
-                echo '<div class="title-wrapper"><h2 class="title">Newsletter</h2></div>
-                    <p>'.__('Praesent pellentesque volutpat ex, vitae auctor lorem pulvinar mollis felis at lacinia.', 'pinplast').'</p>';
+                echo '</div>
+                <div class="newsletter">';
+                    echo '<div class="title-wrapper"><h2 class="title">Newsletter</h2></div>
+                        <p>'.__('Praesent pellentesque volutpat ex, vitae auctor lorem pulvinar mollis felis at lacinia.', 'pinplast').'</p>';
                     echo do_shortcode('[newsletter_form]');
                     echo '<div>';
-                        $menu_id = get_nav_menu_locations()[ 'social' ];
-                        $menu = wp_get_nav_menu_object( $menu_id );
-                        $items = wp_get_nav_menu_items( $menu_id );
+
+                    $menu_id = get_nav_menu_locations()['social'];
+                    $menu = wp_get_nav_menu_object($menu_id);
+                    $items = wp_get_nav_menu_items($menu_id);
+
+                    if (!empty($items)) {
                         echo '<p class="title">' . $menu->name . '</p>';
                         wp_nav_menu(
                             array(
-                                'container' => 'nav', 
-                                'container_class' => 'social', 
+                                'container' => 'nav',
+                                'container_class' => 'social',
                                 'theme_location' => 'social',
-                            ) 
-                        ); 
-                    echo '</div>
-                </div>';
-                }
-                
-                echo '<div></div>
+                            )
+                        );
+                    }
+
+                    echo '</div>';
+                echo '</div>';
+
+                echo '</div>
+                <div></div>
                 <div></div>
             </section>
         </footer>';
