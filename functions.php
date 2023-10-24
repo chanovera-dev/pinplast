32518662bf83456e33d3154aa6a2ae2064a5875e
Chano Vera <github@chanovera.com>
2023-10-23 21:38:16 -0600

    ajustes en el nav right del header

--- functions.php
+++ functions.php
@@ -136,9 +136,12 @@ if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_get_items_count' ) 
     function yith_wcwl_get_items_count() {
       ob_start();
       ?>
+        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+        </svg>
         <a href="<?php echo esc_url( YITH_WCWL()->get_wishlist_url() ); ?>">
           <span class="yith-wcwl-items-count">
-            <i class="yith-wcwl-icon fa fa-heart-o"><?php echo esc_html( yith_wcwl_count_all_products() ); ?></i>
+            <?php echo esc_html( yith_wcwl_count_all_products() ); ?>
           </span>
         </a>
       <?php
