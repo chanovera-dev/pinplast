ad958c5b9063657d798e9c62073cccbcee11c3de
Chano Vera <github@chanovera.com>
2023-10-23 22:11:20 -0600

    se agregan estilos al breadcrumb de woocommerce

--- functions.php
+++ functions.php
@@ -182,3 +182,13 @@ if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_get_items_count' ) 
   
     add_action( 'wp_enqueue_scripts', 'yith_wcwl_enqueue_custom_script', 20 );
   }
+
+  /**
+ * Change the breadcrumb separator
+ */
+add_filter( 'woocommerce_breadcrumb_defaults', 'wcc_change_breadcrumb_delimiter' );
+function wcc_change_breadcrumb_delimiter( $defaults ) {
+	// Change the breadcrumb delimeter from '/' to '>'
+	$defaults['delimiter'] = ' &gt; ';
+	return $defaults;
+}
