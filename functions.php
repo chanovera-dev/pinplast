00335b58513d8f11d43ad5f70301b4a83b88d0c5
Chano Vera <github@chanovera.com>
2023-10-23 22:14:46 -0600

    personalizar el separador del breadcrumb de woocommerce

--- functions.php
+++ functions.php
@@ -189,6 +189,6 @@ if ( defined( 'YITH_WCWL' ) && ! function_exists( 'yith_wcwl_get_items_count' ) 
 add_filter( 'woocommerce_breadcrumb_defaults', 'wcc_change_breadcrumb_delimiter' );
 function wcc_change_breadcrumb_delimiter( $defaults ) {
 	// Change the breadcrumb delimeter from '/' to '>'
-	$defaults['delimiter'] = '  &gt;  ';
+	$defaults['delimiter'] = '&nbsp;&gt;&nbsp;';
 	return $defaults;
 }
