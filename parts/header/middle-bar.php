86a9cfea893144615168803cba26ec799f5adb9d
Chano <github@chanovera.com>
2023-10-25 12:14:32 -0600

    ajuste a customer service

--- parts/header/middle-bar.php
+++ parts/header/middle-bar.php
@@ -5,7 +5,7 @@ echo '<div class="middle-bar-wrapper">
     get_search_form();
     echo '<div class="customer-service">';
         echo '<p>Servicio al cliente</p>';
-        echo '<p><span>' . get_theme_mod('phone_number', '2299216844') . '</span></p>';
+        echo '<p><span>' . get_theme_mod('customer_service', '(229) 921-6844') . '</span></p>';
     echo '</div>
     </section>
 </div>';
