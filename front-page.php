0d73a01e889b4a3dbf3b52d0842e211da62b8d78
Chano <github@chanovera.com>
2023-10-29 15:06:02 -0600

    featured products

--- front-page.php
+++ front-page.php
@@ -3,5 +3,6 @@ get_header();
 echo '<main id="main">';
     include(TEMPLATEPATH . '/parts/frontpage/hero.php');
     include(TEMPLATEPATH . '/parts/frontpage/features.php');
+    include(TEMPLATEPATH . '/parts/frontpage/featured-products.php');
 echo '</main>';
 get_footer();
