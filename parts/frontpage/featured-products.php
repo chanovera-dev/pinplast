6ecd6eb912b841013888600882c553fa930693a9
Chano <github@chanovera.com>
2023-10-29 16:30:50 -0600

    featured products

--- parts/frontpage/featured-products.php
+++ parts/frontpage/featured-products.php
@@ -3,7 +3,7 @@
         <div class="title-wrapper"><h2 class="title"><?php echo __('Productos destacados', 'pinplast'); ?></h2></div>
     </div>
     <div class="section featured-products-list">
-    <?php echo do_shortcode('[featured_products per_page="3" order_by="date" order="desc"]'); ?>
+    <?php echo do_shortcode('[featured_products per_page="10" order_by="date" order="desc"]'); ?>
 
     </div>
 </section>
