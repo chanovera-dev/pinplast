b89f2f01d04742a554763c69756370253eb550f0
Chano <github@chanovera.com>
2023-10-29 12:27:30 -0600

    hero

--- assets/js/hero-slideshow.js
+++ assets/js/hero-slideshow.js
@@ -9,16 +9,19 @@ function left66() {slideshow.style.transform = "translateX(-66.6666%)";}
 
 button1.addEventListener('click', function(){
     left0();
+    button1.classList.add('active');
     button2.classList.remove('active');
     button3.classList.remove('active');
 });
 button2.addEventListener('click', function(){
     left33();
+    button2.classList.add('active');
     button1.classList.remove('active');
     button3.classList.remove('active');
 });
 button3.addEventListener('click', function(){
     left66();
+    button3.classList.add('active');
     button1.classList.remove('active');
     button2.classList.remove('active');
 });
