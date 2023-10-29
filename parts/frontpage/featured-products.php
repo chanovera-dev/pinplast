550a4066e2aac15f8cacf991998cf171013af4c8
Chano <github@chanovera.com>
2023-10-29 15:51:14 -0600

    featured products

--- parts/frontpage/featured-products.php
+++ parts/frontpage/featured-products.php
@@ -3,30 +3,7 @@
         <div class="title-wrapper"><h2 class="title"><?php echo __('Productos destacados', 'pinplast'); ?></h2></div>
     </div>
     <div class="section featured-products-list">
-    <?php
-        // Conexión a la base de datos de WordPress
-        global $wpdb;
-
-        // Obtener los últimos 10 productos destacados de WooCommerce
-        $productos_destacados = $wpdb->get_results(
-            "SELECT p.* 
-            FROM {$wpdb->prefix}posts p
-            INNER JOIN {$wpdb->prefix}postmeta pm ON p.ID = pm.post_id
-            WHERE p.post_type = 'product' 
-            AND p.post_status = 'publish' 
-            AND pm.meta_key = '_featured' 
-            AND pm.meta_value = 'yes'
-            ORDER BY p.post_date DESC 
-            LIMIT 10"
-        );
-
-        // Mostrar los resultados
-        foreach ($productos_destacados as $producto) {
-            echo '<h2>' . $producto->post_title . '</h2>';
-            echo '<p>' . $producto->post_content . '</p>';
-            // Puedes acceder a más información del producto utilizando $producto->ID, $producto->post_meta, etc.
-        }
-        ?>
+    <?php echo do_shortcode('[featured_products per_page="3" order_by="date" order="desc"]'); ?>
 
     </div>
 </section>
