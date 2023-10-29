bf411fd3b0f0bcc1bd68cb653da3382c63d0cc4e
Chano <github@chanovera.com>
2023-10-29 13:01:12 -0600

    hero

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php
 get_header();
 echo '<main id="main">';
-include(TEMPLATEPATH . '/parts/frontpage/hero.php');
-
+    include(TEMPLATEPATH . '/parts/frontpage/hero.php');
+    include(TEMPLATEPATH . '/parts/frontpage/features.php');
 echo '</main>';
 get_footer();
