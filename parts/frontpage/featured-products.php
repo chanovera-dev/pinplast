c17b9dc65afb48d7cf4a4e84df7d3806300df58d
Chano <github@chanovera.com>
2023-10-29 15:47:07 -0600

    featured products

--- parts/frontpage/featured-products.php
+++ parts/frontpage/featured-products.php
@@ -7,22 +7,26 @@
         // Conexión a la base de datos de WordPress
         global $wpdb;
 
-        // Obtener los últimos 10 productos de WooCommerce
-        $productos = $wpdb->get_results(
-            "SELECT * 
-            FROM {$wpdb->prefix}posts 
-            WHERE post_type = 'product' 
-            AND post_status = 'publish' 
-            ORDER BY post_date DESC 
+        // Obtener los últimos 10 productos destacados de WooCommerce
+        $productos_destacados = $wpdb->get_results(
+            "SELECT p.* 
+            FROM {$wpdb->prefix}posts p
+            INNER JOIN {$wpdb->prefix}postmeta pm ON p.ID = pm.post_id
+            WHERE p.post_type = 'product' 
+            AND p.post_status = 'publish' 
+            AND pm.meta_key = '_featured' 
+            AND pm.meta_value = 'yes'
+            ORDER BY p.post_date DESC 
             LIMIT 10"
         );
 
         // Mostrar los resultados
-        foreach ($productos as $producto) {
+        foreach ($productos_destacados as $producto) {
             echo '<h2>' . $producto->post_title . '</h2>';
             echo '<p>' . $producto->post_content . '</p>';
             // Puedes acceder a más información del producto utilizando $producto->ID, $producto->post_meta, etc.
         }
         ?>
+
     </div>
 </section>
