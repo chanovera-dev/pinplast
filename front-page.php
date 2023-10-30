ed76943a6acde6fd3b95fc23f4291db1b86e9deb
Chano <github@chanovera.com>
2023-10-29 20:55:50 -0600

    featured products

--- front-page.php
+++ front-page.php
@@ -4,5 +4,6 @@ echo '<main id="main">';
     include(TEMPLATEPATH . '/parts/frontpage/hero.php');
     include(TEMPLATEPATH . '/parts/frontpage/features.php');
     include(TEMPLATEPATH . '/parts/frontpage/featured-products.php');
+    include(TEMPLATEPATH . '/parts/frontpage/catalog.php');
 echo '</main>';
 get_footer();
---
+++ parts/frontpage/catalog.php
@@ -0,0 +1,7 @@
+<section id="catalog-products" class="container main-content">
+    <div class="section padding-section catalog-products-section">
+        <a class="catalog-link" href="<?php echo home_url('/'); ?>/catalogo">
+            <button>Catálogo</button>
+        </a>
+    </div>
+</section>
