cc74f07f9763b4f244952ed5a8c5e7567583e1d6
Chano <github@chanovera.com>
2023-10-30 11:03:33 -0600

    catalogo

--- parts/frontpage/catalog.php
+++ parts/frontpage/catalog.php
@@ -1,7 +1,11 @@
 <section id="catalog-products" class="container main-content">
     <div class="section padding-section catalog-products-section">
         <a class="catalog-link" href="<?php echo home_url('/'); ?>/catalogo">
-            <button>Compra ahora</button>
+            <div class="tools">
+                <h2>Cientos</h2>
+                <p>Brochas, rodillos, bases, etcétera, etcétera ...</p>
+                <button>Compra ahora</button>
+            </div>
         </a>
     </div>
 </section>
