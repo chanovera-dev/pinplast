631deb2bbbd3730f1bdad454759ab2fdbcea5aa1
Chano <github@chanovera.com>
2023-11-02 13:27:03 -0600

    ajustes a los productos recién llegados

--- parts/frontpage/arrivals.php
+++ parts/frontpage/arrivals.php
@@ -24,9 +24,13 @@
                     ?>  
                     <div class="content">
                         <h3 class="title"><?php the_title(); ?></h3>
-                        <div class="rating">
-                            <?php echo wc_get_rating_html( $average_rating ); ?>
-                        </div>
+                        <?php if ( $average_rating > 0 ) : ?>
+                            <div class="rating">
+                                <?php echo wc_get_rating_html( $average_rating ); ?>
+                            </div>
+                        <?php else : ?>
+                            <span class="no-rating">No hay calificaciones</span>
+                        <?php endif; ?>
                         <?php echo '<span class="price">'. $product->get_price_html() .'</span>'; ?>
                     </div>
                 </a>
