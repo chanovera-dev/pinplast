eb22620448be65f185e41005ae9738e0a5449354
Chano <github@chanovera.com>
2023-11-04 07:42:40 -0600

    listas

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@ echo '<main id="main">';
     include(TEMPLATEPATH . '/parts/frontpage/featured-products.php');
     include(TEMPLATEPATH . '/parts/frontpage/catalog.php');
     include(TEMPLATEPATH . '/parts/frontpage/bestsellers.php');
-    include(TEMPLATEPATH . '/parts/frontpage/categories.php');
+    // include(TEMPLATEPATH . '/parts/frontpage/categories.php');
     include(TEMPLATEPATH . '/parts/frontpage/arrivals.php');
     if ( get_posts() == null ) : else: include(TEMPLATEPATH . '/parts/frontpage/blog.php'); endif;
     include(TEMPLATEPATH . '/parts/frontpage/products.php');
