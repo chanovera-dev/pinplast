3b3d9eca86ace7830aaa3338e18be4fc97c39df7
Chano <github@chanovera.com>
2023-11-05 17:54:59 -0600

    ajustes de estilos al menú mobile

--- functions/colors.php
+++ functions/colors.php
@@ -6,13 +6,18 @@ function theme_pinplast_custom_colors() {
             :root{
                 --background-color-body:#fff;
                 --color-text:#000;
+                --attenuated-text:#b3b3b3;
+                --attenuated-icon:#b3b3b3;
+                --attenuated-line:#b3b3b3;
                 --color-links:#1a66ff;
+                --yellow:#ffd333;
                 --link-focus:0 0 0 .1rem #ffc107;
 
                 /* cabecera */
                 --color-header:#3d464d;
                 --border-color:#ebebeb;
                 --menu-mobile--button__active:#999;
+                --background-color--submenu:#f7f7f7;
 
                 /* formularios */
                 --background-color-input:#fff;
