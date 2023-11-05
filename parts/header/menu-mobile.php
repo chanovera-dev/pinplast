af4584e0c56f6668d6849393e7354fd3903db6f5
Chano <github@chanovera.com>
2023-11-05 14:00:18 -0600

    se agrega el menú mobile

--- parts/header/menu-mobile.php
+++ parts/header/menu-mobile.php
@@ -1,11 +1,9 @@
 <?php
-    echo '<div id="menu-mobile--wrapper" class="menu-mobile--wrapper">';   
-        wp_nav_menu(
-            array(
-                'container' => 'nav',
-                'container_id' => 'menu-mobile',
-                'container_class' => 'mobile', 
-                'theme_location' => 'mobile',
-            ) 
-        );
-    echo '</div>';
+    wp_nav_menu(
+        array(
+            'container' => 'nav',
+            'container_id' => 'menu-mobile--wrapper',
+            'container_class' => 'mobile', 
+            'theme_location' => 'mobile',
+        ) 
+    );
