8e69abeda4f0c4cf3cddb6200dfa1ec46f798d2e
Chano <github@chanovera.com>
2023-11-05 19:37:12 -0600

    ajustes de estilos al menú mobile

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,19 +3,24 @@ function myFunction(x) {
     let nav = document.getElementById("menu-mobile--wrapper");
     let body = document.getElementById("body");
     let header = document.getElementById("main-header");
+    let main = document.getElementById("#main");
     x.classList.toggle("change");
 
     if (!nav.classList.contains("active")) {
         nav.classList.add("active");
         body.classList.add("menu-active");
         header.classList.add("menu-active");
+        main.classList.add("menu-active");
     } else {
         nav.classList.remove("active");
         body.classList.remove("menu-active");
         header.classList.add("menu-inactive");
+        main.classList.add("menu-inactive");
         setTimeout(function() {
             header.classList.remove("menu-inactive");
             header.classList.remove("menu-active");
+            main.classList.remove("menu-inactive");
+            main.classList.remove("menu-active");
         }, 300);
     }
 }
