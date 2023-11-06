2117c43cfe197c7ea59daedb314aa68b503b9328
Chano <github@chanovera.com>
2023-11-05 19:03:18 -0600

    ajustes de estilos al menú mobile

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,18 +1,28 @@
 /* arreglo del botón del menú y del menú */
 function myFunction(x) {
     let nav = document.getElementById("menu-mobile--wrapper");
-    let body = document.getElementById("body");
+    let header = document.getElementById("main-header");
+    let main = document.getElementById("main");
+    let footer = document.getElementById("main-footer");
     x.classList.toggle("change");
 
     if (!nav.classList.contains("active")) {
         nav.classList.add("active");
-        body.classList.add("menu-active");
+        header.classList.add("menu-active");
+        main.classList.add("menu-active");
+        footer.classList.add("menu-active");
     } else {
         nav.classList.remove("active");
-        body.classList.add("menu-inactive");
+        header.classList.add("menu-inactive");
+        main.classList.add("menu-inactive");
+        footer.classList.add("menu-inactive");
         setTimeout(function() {
-            body.classList.remove("menu-inactive");
-            body.classList.remove("menu-active");
+            header.classList.remove("menu-inactive");
+            header.classList.remove("menu-active");
+            main.classList.remove("menu-inactive");
+            main.classList.remove("menu-active");
+            footer.classList.remove("menu-inactive");
+            footer.classList.remove("menu-active");
         }, 300);
     }
 }
