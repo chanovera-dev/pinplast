8c5c6a3b2ff52bca8467393c8e5538fa5930bcd1
Chano <github@chanovera.com>
2023-11-06 07:32:46 -0600

    icon submenu

--- functions/icons.php
+++ functions/icons.php
@@ -4,7 +4,7 @@ function pinplast_theme_custom_icons() {
     ?>
         <style>
             /* iconos en el menú */
-            .menu-mobile--wrapper > ul li.menu-item-has-children a:after{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/chevron.svg');}
+            .menu-mobile--wrapper > ul li.menu-item-has-children > a:after{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/chevron.svg');}
             
             /* iconos de redes sociales */
             .social .menu li a[href*="facebook"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/facebook.svg');}
