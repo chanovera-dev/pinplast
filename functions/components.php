f21828eed6136a5fcec00fd38492779ed1095bd2
Chano <github@chanovera.com>
2023-11-05 21:04:12 -0600

    address

---
+++ functions/components.php
@@ -0,0 +1,10 @@
+<?php
+
+// Estilos para la página frontal
+function frontpage_styles() {
+    if ( is_front_page() or is_page_template('front-page.php') ) {
+        wp_dequeue_style( 'wp-block-library' );
+        wp_enqueue_style( 'widgets-styles', get_template_directory_uri() . '/assets/css/widgets.css' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'frontpage_styles' );
