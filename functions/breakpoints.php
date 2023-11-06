f258de6ab7fc5798424f01c366881ced84a2896b
Chano <github@chanovera.com>
2023-11-06 10:42:47 -0600

    scroll sobre el header

--- functions/breakpoints.php
+++ functions/breakpoints.php
@@ -5,6 +5,7 @@ function pinplast_theme_custom_breakpoints() {
         <style>
             :root{
                 --breakpoint:min(100% - 3rem, 576px);
+                --main-padding-top:5.4rem;
             }
 
             @media(min-width:768px){
