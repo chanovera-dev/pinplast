a95e30ed3ab188b196274af4aa31c51cb2161b4d
Chano <github@chanovera.com>
2023-11-05 20:15:40 -0600

    ajustes de estilos al menú mobile

--- functions/icons.php
+++ functions/icons.php
@@ -3,7 +3,12 @@
 function pinplast_theme_custom_icons() {
     ?>
         <style>
-            
+            /* iconos de redes sociales */
+            .social .menu li a[href*="facebook"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/facebook.svg');}
+            .social .menu li a[href*="twitter"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/twitter.svg');}
+            .social .menu li a[href*="youtube"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/youtube.svg');}
+            .social .menu li a[href*="instagram"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/instagram.svg');}
+            .social .menu li a[href*="sites"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/google.svg');}
         </style>
     <?php
 }
