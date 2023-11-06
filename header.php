c2428b96d90e45672b56fb11ae81f2944c3045a1
Chano <github@chanovera.com>
2023-11-06 09:38:57 -0600

    scroll sobre el header

--- header.php
+++ header.php
@@ -10,13 +10,15 @@ echo '<!DOCTYPE html>
     echo '</head>
     <body id="body" '; body_class(); echo '>
         <header id="main-header" class="container main-header">
-            <section id="responsive-header" class="section header-content">';
-                include(TEMPLATEPATH . '/parts/header/menu-mobile.php');
-                include(TEMPLATEPATH . '/parts/header/menu-button.php');
-                include(TEMPLATEPATH . '/parts/header/brand.php');
-                get_search_form();
-                include(TEMPLATEPATH . '/parts/header/lists.php');
-            echo '</section>
+            <div class="responsive-header-wrapper">
+                <section id="responsive-header" class="section header-content">';
+                    include(TEMPLATEPATH . '/parts/header/menu-mobile.php');
+                    include(TEMPLATEPATH . '/parts/header/menu-button.php');
+                    include(TEMPLATEPATH . '/parts/header/brand.php');
+                    get_search_form();
+                    include(TEMPLATEPATH . '/parts/header/lists.php');
+                echo '</section>
+            </div>
             <section id="desktop-header" class="section header-content">';
             include(TEMPLATEPATH . '/parts/header/top-bar.php');
             include(TEMPLATEPATH . '/parts/header/middle-bar.php');
