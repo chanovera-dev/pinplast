0b71ce249072c0dd29afdef3a792166d677979e7
Chano <github@chanovera.com>
2023-11-07 09:30:08 -0600

    frontpage - hero

--- functions/components.php
+++ functions/components.php
@@ -4,7 +4,11 @@
 function frontpage_styles() {
     if ( is_front_page() or is_page_template('front-page.php') ) {
         wp_dequeue_style( 'wp-block-library' );
+        /* hero section */
         wp_enqueue_style( 'frontpage-styles', get_template_directory_uri() . '/assets/css/frontpage/hero.css' );
+        wp_enqueue_script( 'hero-slideshow', get_template_directory_uri() . '/assets/js/hero-slideshow.js', array(), '1.0', true );
+        
+        /* widgets */
         wp_enqueue_style( 'widgets-styles', get_template_directory_uri() . '/assets/css/widgets.css' );
     }
 }
