a25507bea22cd5bbf9949ccb014afa2660a7613e
Chano <github@chanovera.com>
2023-11-09 16:48:58 -0600

    features

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 get_header();
 echo '<main id="main">';
     include(TEMPLATEPATH . '/parts/frontpage/hero.php');
-    // include(TEMPLATEPATH . '/parts/frontpage/features.php');
+    include(TEMPLATEPATH . '/parts/frontpage/features.php');
     // include(TEMPLATEPATH . '/parts/frontpage/featured-products.php');
     // include(TEMPLATEPATH . '/parts/frontpage/catalog.php');
     // include(TEMPLATEPATH . '/parts/frontpage/bestsellers.php');
--- functions/components.php
+++ functions/components.php
@@ -7,7 +7,8 @@ function frontpage_styles() {
         /* hero section */
         wp_enqueue_style( 'frontpage-styles', get_template_directory_uri() . '/assets/css/frontpage/hero.css' );
         wp_enqueue_script( 'hero-slideshow', get_template_directory_uri() . '/assets/js/hero-slideshow.js', array(), '1.0', true );
-        
+        /* features */
+        wp_enqueue_style( 'features-styles', get_template_directory_uri() . '/assets/css/frontpage/features.css' );
         /* widgets */
         wp_enqueue_style( 'widgets-styles', get_template_directory_uri() . '/assets/css/widgets.css' );
     }
