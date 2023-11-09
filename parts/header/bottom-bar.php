f5745477abdfc1b858b1e43d236ae76b5092c5ba
Chano <github@chanovera.com>
2023-11-09 13:06:46 -0600

    departmens button

--- parts/header/bottom-bar.php
+++ parts/header/bottom-bar.php
@@ -1,7 +1,16 @@
 <?php
 echo '<div class="bottom-bar-wrapper">
     <section class="section bottom-bar">';
-        echo '<div class=""></div>';
+        echo '<div class="">
+            <button class="departments-button">
+                <div class="bars">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                </div>
+                Comprar por categoría
+            </button>
+        </div>';
         wp_nav_menu(
             array(
                 'container' => 'nav', 
