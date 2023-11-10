4b69049a1cb188501ccbbfb443260889841fad59
Chano <github@chanovera.com>
2023-11-09 21:49:40 -0600

    featured-products

--- parts/frontpage/featured-products.php
+++ parts/frontpage/featured-products.php
@@ -3,10 +3,14 @@
         <h2 class="title"><?php echo __('Productos destacados', 'pinplast'); ?></h2>
         <div class="slideshow-buttons__featured-products">
             <button id="backward-button__featured-products" class="featured-products--button">
-
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                </svg>
             </button>
             <button id="forward-button__featured-products" class="featured-products--button">
-
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
             </button>
         </div>
     </div>
