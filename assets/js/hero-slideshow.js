89cca3ce20664aeb84e726f171616b0846998564
Chano <github@chanovera.com>
2023-11-09 22:08:31 -0600

    featured-products

--- assets/js/hero-slideshow.js
+++ assets/js/hero-slideshow.js
@@ -34,41 +34,44 @@ const nextButton = document.getElementById('forward-button__featured-products');
 let currentImageIndex = 0;
 
 function updateSlideshow() {
-  prevButton.disabled = true;
-  const translateX = -12.5 * currentImageIndex;
-  slideshowContainer.style.transform = `translateX(${translateX}%)`;
+    const translateX = -12.5 * currentImageIndex;
+    slideshowContainer.style.transform = `translateX(${translateX}%)`;
 
-  if (currentImageIndex > 0) {
-    prevButton.disabled = false;
+    if (currentImageIndex === 0) {
+      prevButton.disabled = true;
+      prevButton.classList.add('disable');
+    } else {
+      prevButton.disabled = false;
+      prevButton.classList.remove('disable');
+    }
+
+    switch (true) {
+        case window.innerWidth >= 1024:
+            numero = 4;
+            break;
+        case window.innerWidth >= 768:
+            numero = 5;
+            break;
+        default:
+            numero = 7;
+            break;
+    }
+      
+    if (currentImageIndex === numero) {
+        nextButton.disabled = true;
+        nextButton.classList.add('disable');
+    } else {
+        nextButton.disabled = false;
+        nextButton.classList.remove('disable');
+    }
   }
 
-  switch (true) {
-    case window.innerWidth >= 1024:
-      numero = 4;
-      break;
-    case window.innerWidth >= 768:
-      numero = 5;
-      break;
-    default:
-      numero = 7;
-      break;
-  }
+  prevButton.addEventListener('click', () => {
+    currentImageIndex--;
+    updateSlideshow();
+  });
 
-  if (currentImageIndex === numero) {
-    nextButton.disabled = true;
-    nextButton.classList.add('disable');
-  } else {
-    nextButton.disabled = false;
-    nextButton.classList.remove('disable');
-  }
-}
-
-prevButton.addEventListener('click', () => {
-  currentImageIndex--;
-  updateSlideshow();
-});
-
-nextButton.addEventListener('click', () => {
-  currentImageIndex++;
-  updateSlideshow();
-});
+  nextButton.addEventListener('click', () => {
+    currentImageIndex++;
+    updateSlideshow();
+  });
