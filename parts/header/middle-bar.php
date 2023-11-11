614eaf104dab3519f6663ab2a684595fd4d5e203
Chano <github@chanovera.com>
2023-11-11 14:20:20 -0600

    número con link de llamada en el header

--- parts/header/middle-bar.php
+++ parts/header/middle-bar.php
@@ -5,7 +5,7 @@ echo '<div class="middle-bar-wrapper">
     get_search_form();
     echo '<div class="customer-service">';
         echo '<p>Servicio al cliente</p>';
-        echo '<p><span>' . get_theme_mod('customer_service', '(229) 921-6844') . '</span></p>';
+        echo '<a href="tel:'.get_theme_mod('customer_service', '2299216844').'"><p><span>' . get_theme_mod('customer_service', '(229) 921-6844') . '</span></p></a>';
     echo '</div>
     </section>
 </div>';
