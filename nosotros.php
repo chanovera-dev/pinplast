e78f09a2781f437cb3739e015e39b46ad1404644
Chano <github@chanovera.com>
2023-11-12 13:33:01 -0600

    template nosotros | css

--- nosotros.php
+++ nosotros.php
@@ -3,7 +3,7 @@ get_header();
 echo '<main id="main">
     <div class="container about-us__image"></div>
     <div class="container">
-        <section class="section padding-section">';
+        <section class="section nosotros-section">';
             the_title('<h2 class="title">', '</h2>');
             the_content();
         echo '</section>
