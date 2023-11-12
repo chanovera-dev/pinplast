1129971dbc4e2a9a90fda1cf43821ea9c4472f58
Chano <github@chanovera.com>
2023-11-12 09:22:33 -0600

    condicional en caso de que los menús en la top bar estén vacíos

--- parts/header/top-bar.php
+++ parts/header/top-bar.php
@@ -1,13 +1,22 @@
 <?php
-echo '<div class="top-bar-wrapper">
-    <section class="section top-bar">';
+$secondary_menu = wp_get_nav_menu_items('secondary');
+$tertiary_menu = wp_get_nav_menu_items('tertiary');
+
+if ($secondary_menu || $tertiary_menu) {
+    echo '<div class="top-bar-wrapper">
+        <section class="section top-bar">';
+
+    if ($secondary_menu) {
         wp_nav_menu(
             array(
                 'container' => 'nav', 
                 'container_class' => 'secondary', 
                 'theme_location' => 'secondary',
-            ) 
+            )
         );
+    }
+
+    if ($tertiary_menu) {
         wp_nav_menu(
             array(
                 'container' => 'nav', 
@@ -15,5 +24,9 @@ echo '<div class="top-bar-wrapper">
                 'theme_location' => 'tertiary',
             ) 
         );
+    }
+
     echo '</section>
-</div>';
+    </div>';
+}
+?>
