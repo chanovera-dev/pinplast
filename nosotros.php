c7ed189a256f27ecd16e0a6df19aaae597d52752
Chano <github@chanovera.com>
2023-11-12 16:38:05 -0600

    page

--- nosotros.php
+++ nosotros.php
@@ -4,7 +4,7 @@ echo '<main id="main">
     <div class="container about-us__image"></div>
     <div class="container">
         <section class="section nosotros-section">';
-            the_title('<h2 class="title">', '</h2>');
+            the_title('<h1 class="title">', '</h1>');
             the_content();
         echo '</section>
     </div>
---
+++ page.php
@@ -0,0 +1,12 @@
+<?php
+    get_header();
+    echo '<main id="main">
+        <section class="container main-content">
+            <div class="section">';get_breadcrumb(); echo '</div>
+            <article class="section padding-section">';
+                the_title('<h1 class="title">', '</h1>');
+                the_content();
+            echo '</article>
+        </section>
+    </main>';
+    get_footer();
