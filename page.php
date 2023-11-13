dcf7d2bc091ec1e65b7f3d281366a1a3ca30220b
Chano <github@chanovera.com>
2023-11-13 16:19:55 -0600

    page

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
     echo '<main id="main">
         <section class="container main-content">
             <div class="container"><section class="section"><div class="breadcrumbs">';get_breadcrumb(); echo '</div></section></div>
-            <article class="section padding-section">';
+            <article class="section content-section border-section">';
                 the_title('<h1 class="title">', '</h1>');
                 the_content();
             echo '</article>
