c1f050a1482d88315175dc075672867c8fd76a36
Chano <github@chanovera.com>
2023-11-18 10:51:53 -0600

    single product

--- single-product.php
+++ single-product.php
@@ -1,7 +1,6 @@
 <?php 
 get_header();
 echo '<main id="main">';
-include(TEMPLATEPATH . '/parts/sections/title-woocommerce.php');
     if ( have_posts() ){    
         while( have_posts() ){  
             the_post();    
