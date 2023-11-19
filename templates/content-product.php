2cba3813b36fa53058b776bbcca95b1741de1530
Chano <github@chanovera.com>
2023-11-19 12:51:26 -0600

    single product | galería - miniaturas

--- templates/content-product.php
+++ templates/content-product.php
@@ -38,9 +38,7 @@
                             if ( $post_thumbnail_id ) {
                                 $html = wc_get_gallery_image_html( $post_thumbnail_id, true );
                             } else {
-                                $html  = '<div class="woocommerce-product-gallery__image--placeholder">';
-                                $html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
-                                $html .= '</div>';
+                                $html = wc_get_gallery_image_html( $post_thumbnail_id, true );
                             }
                     
                             echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
