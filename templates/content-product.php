c3faab51bb570dc55963c6779d2501cb52fab132
Chano <github@chanovera.com>
2023-11-19 12:01:31 -0600

    single product | galería - miniaturas

--- templates/content-product.php
+++ templates/content-product.php
@@ -10,24 +10,7 @@
     <section class="container container-data-product">
         <div class="section padding-section data-product">
             <div class="product-gallery">
-                <?php 
-                    defined( 'ABSPATH' ) || exit;
-
-                    // Note: `wc_get_gallery_image_html` was added in WC 3.3.2 and did not exist prior. This check protects against theme overrides being used on older versions of WC.
-                    if ( ! function_exists( 'wc_get_gallery_image_html' ) ) {
-                        return;
-                    }
-                    
-                    global $product;
-                    
-                    $attachment_ids = $product->get_gallery_image_ids();
-                    
-                    if ( $attachment_ids && $product->get_image_id() ) {
-                        foreach ( $attachment_ids as $attachment_id ) {
-                            echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html( $attachment_id ), $attachment_id ); // phpcs:disable WordPress.XSS.EscapeOutput.OutputNotEscaped
-                        }
-                    }
-                ?>
+                <?php do_action( 'woocommerce_before_single_product_summary' ); ?>
             </div>
             <div class="product-summary">
                 <?php 
