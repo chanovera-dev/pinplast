6edc7729f3dc7ca4410eeb832aaf32827b6456b0
Chano <github@chanovera.com>
2023-11-19 19:04:51 -0600

    single product | summary -- añadir al carrito

--- functions/woocommerce/woocommerce-components.php
+++ functions/woocommerce/woocommerce-components.php
@@ -9,6 +9,8 @@ function template_redirect_action() {
             $product = wc_get_product( $post->ID );
             $tipo    = $product->get_type();
             wp_enqueue_style( 'single-product-styles', get_template_directory_uri() . '/assets/css/woocommerce/single-product.css' );
+            // JS de ajustes
+            wp_enqueue_script( 'ajustes', get_template_directory_uri() . '/assets/js/single-product.js', '', 1, true );
             /* estilos css para los formularios */
             wp_enqueue_style( 'forms-styles', get_template_directory_uri() . '/assets/css/forms.css' ); 
             return array_merge( $classes, array( $tipo ) );
