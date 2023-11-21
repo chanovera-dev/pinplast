392b0682f40e7c7f2bd37d495ab939badd21c3a3
Chano <github@chanovera.com>
2023-11-21 11:48:20 -0600

    404

--- functions/components.php
+++ functions/components.php
@@ -60,6 +60,8 @@ add_action( 'wp_enqueue_scripts', 'contact_styles' );
 function page404_styles() {
     if ( is_404() ) {
         wp_enqueue_style( 'error404-styles', get_template_directory_uri() . '/assets/css/error404.css' );
+        /* forms */
+        wp_enqueue_style( 'forms-styles', get_template_directory_uri() . '/assets/css/forms.css' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'page404_styles' );
