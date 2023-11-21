a771fbba37405e11ce55ab441ab228d11667bba1
Chano <github@chanovera.com>
2023-11-21 12:54:44 -0600

    carrito

--- cart.php
+++ cart.php
@@ -14,12 +14,9 @@
             </div>
         </section>';
         echo 
-        '<section class="container main-content"><ul class="section padding-section product-list">';
-        while( have_posts() ){         
-            the_post();
-            get_template_part( 'templates/content', 'products' );          
-        }
-        echo '</ul>';      
+        '<section class="container main-content"><div class="section padding-section product-list">';
+        echo do_shortcode('[woocommerce_cart]');
+        echo '</div>';      
     } else {
         echo '<section class="container"><div class="section">' . '<p>' . __('Actualmente no hay artículos en esta tienda', 'pinplast') . '</p>' . '</div>';
     }
