5dac1522f3dc4b61bf69197e393b115a7350a05b
Chano <github@chanovera.com>
2023-11-20 21:40:28 -0600

    product

--- templates/content-products.php
+++ templates/content-products.php
@@ -23,21 +23,22 @@
     if ( empty( $product ) || ! $product->is_visible() ) {
         return;
     }
-    ?>
-    <li <?php wc_product_class( '', $product ); ?>>
-        <div class="card">
-            <?php
+    echo '
+    <li'; wc_product_class( '', $product ); echo '>
+        <div class="card">';
+            
                 if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, array(200,200));
                 else echo '<img src="'.wc_placeholder_img_src().'" alt="Placeholder" loading="lazy">';
-            ?>  
+            echo '  
             <div class="content">
-                <a href="<?php echo esc_url( get_permalink( $loop->post->ID ) ); ?>" class="permalink"><h3 class="title"><?php the_title(); ?></h3></a>
-                <?php if ( wc_get_product( $loop->post->ID )->get_average_rating() > 0 ) : ?>
-                <div class="rating">
-                    <?php echo wc_get_rating_html( wc_get_product( $loop->post->ID )->get_average_rating() ); ?>
-                    <span class="votes-rating"><?php echo $rating_count . ' votos'; ?></span>
-                </div>
-                <?php else : ?>
+                <a href="' . esc_url( get_permalink( $loop->post->ID ) ) . '" class="permalink"><h3 class="title">'; the_title(); echo '</h3></a>';
+                if ( $average_rating > 0 ) :
+                echo '<div class="rating">';
+                    echo wc_get_rating_html( $average_rating );
+                    echo '<span class="votes-rating">' . $rating_count . ' votos' . '</span>
+                </div>';
+                else :
+                    echo '
                     <div class="rating">
                         <div class="star-rating" role="img" aria-label="Valorado en 0 de 5">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16"><path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path></svg>
@@ -46,17 +47,16 @@
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16"><path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path></svg>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16"><path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"></path></svg>
                         </div>
-                        <span class="votes-rating"><?php echo '0 votos'; ?></span>
-                </div>
-                <?php endif; ?> 
-                <?php echo '<div class="price">'. $product->get_price_html() .'</div>'; ?>
+                        <span class="votes-rating">0 votos</span>
+                </div>';
+                endif; 
+                echo '<div class="price">'. $product->get_price_html() .'</div>
             </div>
             <div class="add-to-cart_add-to-wishlist--buttons">
-                <div class="buttons">
-                    <?php
+                <div class="buttons">';
                         woocommerce_template_loop_add_to_cart();
                         echo do_shortcode('[yith_wcwl_add_to_wishlist]');
-                    ?>
+                    echo '
                 </div>
             </div>
         </div>
