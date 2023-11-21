c6e54766f02e97ea2714cc0ea8f2508add9befa1
Chano <github@chanovera.com>
2023-11-20 22:30:01 -0600

    product

--- functions/woocommerce/woocommerce-components.php
+++ functions/woocommerce/woocommerce-components.php
@@ -20,7 +20,7 @@ function template_redirect_action() {
 
 // Estilos para los archivos de la tienda
 function shop_styles() {
-    if ( is_shop() ) {
+    if ( is_shop() || is_product_category() ) {
         wp_dequeue_style( 'wp-block-library' );
         wp_enqueue_style( 'shop-styles', get_template_directory_uri() . '/assets/css/woocommerce/shop.css' );
         /* listas */
@@ -28,14 +28,3 @@ function shop_styles() {
     }
 }
 add_action( 'wp_enqueue_scripts', 'shop_styles' );
-
-// Estilos para los archivos de la tienda
-function category_styles() {
-    if ( is_category() ) {
-        wp_dequeue_style( 'wp-block-library' );
-        wp_enqueue_style( 'shop-styles', get_template_directory_uri() . '/assets/css/woocommerce/shop.css' );
-        /* listas */
-        wp_enqueue_style( 'lists-styles', get_template_directory_uri() . '/assets/css/frontpage/lists.css' );
-    }
-}
-add_action( 'wp_enqueue_scripts', 'category_styles' );
