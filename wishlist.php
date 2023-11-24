01100f0e8a0febf4f8b90b01f247ebc190ca267a
Chano <github@chanovera.com>
2023-11-24 07:38:03 -0600

    carrito

--- wishlist.php
+++ wishlist.php
@@ -4,11 +4,7 @@
     echo 
     '<main id="main">
         <section class="container">
-            <div class="section">';
-                woocommerce_breadcrumb();
-                the_title('<h1>', '</h1>');
-                echo '
-            </div>
+            
         </section>';
         echo 
         '<section class="container main-content">
