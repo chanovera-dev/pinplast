07b2bd9b6435a5671f969ecdc1a7c4a50916b09e
Chano <github@chanovera.com>
2023-11-25 15:17:42 -0600

    condicional en productos destacados

--- parts/frontpage/featured-products.php
+++ parts/frontpage/featured-products.php
@@ -38,6 +38,7 @@ if (empty($productos_destacados)) {
                             'post_type' => 'product',
                             'post_status' => 'publish',
                             'key' => '_featured',
+                            'featured' => true,
                             'posts_per_page' => 8,
                             'orderby' => 'date',
                             'order'   => 'DESC'
