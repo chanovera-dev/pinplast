080ef1f8996ad165188e8b254366128ff48c8e63
Chano <github@chanovera.com>
2023-11-26 11:33:27 -0600

    paginación

--- archive-product.php
+++ archive-product.php
@@ -14,15 +14,20 @@
             </div>
         </section>';
         echo 
-        '<section class="container main-content"><ul class="section padding-section product-list">';
-        while( have_posts() ){         
-            the_post();
-            get_template_part( 'templates/content', 'products' );          
+        '<section class="container main-content">
+            <ul class="section padding-section product-list">';
+            while( have_posts() ){         
+                the_post();
+                get_template_part( 'templates/content', 'products' );          
+            }
+        echo
+        '</ul>';
+            
+        } else {
+            echo '<section class="container"><div class="section">' . '<p>' . esc_html__('Actualmente no hay artículos en esta tienda', 'pinplast') . '</p>' . '</div>';
         }
-        echo '</ul>';
-        the_posts_pagination();    
-    } else {
-        echo '<section class="container"><div class="section">' . '<p>' . esc_html__('Actualmente no hay artículos en esta tienda', 'pinplast') . '</p>' . '</div>';
-    }
-    echo '</section></main>';
+        the_posts_pagination();
+        echo 
+        '</section>
+    </main>';
     get_footer();
