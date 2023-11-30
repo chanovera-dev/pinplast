47e3850d5005e807b4e9e2ef6eb57e8d9002c2d8
Chano <github@chanovera.com>
2023-11-29 19:23:33 -0600

    prueba para svg4everybody | fuente stroyka

--- parts/frontpage/features.php
+++ parts/frontpage/features.php
@@ -11,21 +11,21 @@
                 </td>
                 <td class="feature-cell">
                     <svg width="48px" height="48px">
-                        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/themes/stroyka/images/sprite.svg#fi-24-hours-48"></use>
+                        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#fi-24-hours-48"></use>
                     </svg>
                     <h2>WhatsApp</h2>
                     <p>Escríbemos tus dudas</p>
                 </td>
                 <td class="feature-cell">
                     <svg width="48px" height="48px">
-                        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/themes/stroyka/images/sprite.svg#fi-payment-security-48"></use>
+                        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#fi-payment-security-48"></use>
                     </svg>
                     <h2>100% seguridad</h2>
                     <p>Tus compras son seguras</p>
                 </td>
                 <td class="feature-cell">
                     <svg width="48px" height="48px">
-                        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/themes/stroyka/images/sprite.svg#fi-tag-48"></use>
+                        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#fi-tag-48"></use>
                     </svg>
                     <h2>Promociones</h2>
                     <p>Descuentos imperdibles</p>
