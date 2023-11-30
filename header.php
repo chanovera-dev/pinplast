c5e0efcc30124e6109c0ee48a52249df649a6459
Chano <github@chanovera.com>
2023-11-30 12:25:55 -0600

    archivos mínimos

--- header.php
+++ header.php
@@ -2,14 +2,15 @@
 
 echo '
 <!DOCTYPE html>
-<html '.language_attributes(). '>
+<html '; language_attributes(); echo '>
     <head>
         <meta charset="'; bloginfo( 'charset' ); echo '">
         <meta name="description" content="'; bloginfo( 'description' ); echo '">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">';
         wp_head();
-    echo '</head>
+    echo '
+    </head>
     <body id="body" '; body_class(); echo '>
         <header id="main-header" class="container main-header">
             <section id="responsive-header">';
