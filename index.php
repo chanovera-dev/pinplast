6291852cacd1a00b1aa6cfd3405712a8285980ac
Chano <github@chanovera.com>
2023-11-30 12:21:41 -0600

    archivos mínimos

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    get_header();
+
+    echo '
+    <main id="main">
+        <div class="container">
+            <section class="section">
+                <h1>'.esc_html__('Bienvenidos al index.php de pinplast.com.mx', 'pinplast').'</h1>
+            </section>
+        </div>
+    </main>
+    ';
+    
+    get_footer();
