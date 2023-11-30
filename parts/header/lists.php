6153204dd283fed7275801e16a433166fa75b145
Chano <github@chanovera.com>
2023-11-29 19:32:17 -0600

    prueba para svg4everybody | fuente stroyka

--- parts/header/lists.php
+++ parts/header/lists.php
@@ -8,8 +8,8 @@ echo '<ul class="lists">';
     } else {}
     
     echo '<li>
-        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
-            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        <svg width="20px" height="20px">
+            <use xlink:href="'.get_template_directory_uri().'/assets/img/sprite.svg#cart-20"></use>
         </svg>
         <a class="counter" href="'; echo wc_get_cart_url(); echo '" title="'; _e( 'View your shopping cart' ); echo '">'; echo sprintf ( _n( '%d', '%d', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); echo '</a>
     </li>';
