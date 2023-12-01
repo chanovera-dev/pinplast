d5ddb98148f1b531604f07164067859dfb30e6dc
Chano <github@chanovera.com>
2023-12-01 00:21:39 -0600

    menu mobile

--- parts/header/menu-mobile.php
+++ parts/header/menu-mobile.php
@@ -16,8 +16,6 @@
         wp_nav_menu(
             array(
                 'container' => 'nav',
-                'container_id' => 'menu-mobile--wrapper',
-                'container_class' => 'menu-mobile--wrapper', 
                 'theme_location' => 'mobile',
             ) 
         );
