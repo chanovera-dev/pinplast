5a0f9fd808124afb75bb7e297bad6979349264d7
Chano <github@chanovera.com>
2023-11-30 18:22:17 -0600

    header | css

--- parts/header/attachments.php
+++ parts/header/attachments.php
@@ -9,7 +9,7 @@ echo '<ul class="attachment-list">';
     
     echo '<li>
         <a class="counter" href="'; echo wc_get_cart_url(); echo '" title="'; _e( 'View your shopping cart' ); echo '">
-            <svg width="22px" height="22px">
+            <svg width="20px" height="20px">
                 <use xlink:href="'.get_template_directory_uri().'/assets/img/sprite.svg#cart-20"></use>
             </svg>
             <span class="cart-items-count">'; echo sprintf ( _n( '%d', '%d', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() );  echo'</span>
