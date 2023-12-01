50fcd7ccdeddd76db826f9d36963ef64f66ab272
Chano <github@chanovera.com>
2023-12-01 02:17:07 -0600

    menu mobile

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform-wrapper" class="searchform-wrapper" action="<?php echo home_url( '/' ); ?>">
 	<div class="searchform">
-		<input type="text" value="" name="s" id="s" class="input-search" placeholder="<?php esc_html__('Buscar', 'pinplast'); ?>">
+		<input type="text" value="" name="s" id="s" class="input-search" placeholder="<?= __('Buscar', 'pinplast'); ?>">
         <div  id="search-buttons">
             <svg width="20px" height="20px" id="open-searchbar--button">
                 <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#search-20"></use>
