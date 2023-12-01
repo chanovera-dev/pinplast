bf4ad3d762fee512f8bb9a19b8308d9c92d1ca7d
Chano <github@chanovera.com>
2023-12-01 02:32:48 -0600

    menu mobile

--- searchform.php
+++ searchform.php
@@ -2,12 +2,17 @@
 	<div class="searchform">
 		<input type="text" value="" name="s" id="s" class="input-search" placeholder="<?= __('Buscar', 'pinplast'); ?>">
         <div  id="search-buttons">
-            <svg width="20px" height="20px" id="open-searchbar--button">
-                <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#search-20"></use>
-            </svg>
-            <svg width="20px" height="20px" id="close-searchbar--button">
-                <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#cross-20"></use>
-            </svg>
+            <button id="open-searchbar--button">
+                <svg width="20px" height="20px">
+                    <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#search-20"></use>
+                </svg>
+            </button>
+            <button id="close-searchbar--button">
+                <svg width="20px" height="20px">
+                    <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#cross-20"></use>
+                </svg>
+            </button>
+            
         </div>
 	</div>
 </form>
