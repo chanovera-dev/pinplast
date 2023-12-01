fc0dd697eea148e982616b8624f5af6703e68e83
Chano <github@chanovera.com>
2023-11-30 22:40:10 -0600

    button menu | css

--- searchform.php
+++ searchform.php
@@ -1,7 +1,7 @@
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
-	<div class="searchform-wrapper">
+<form role="search" method="get" id="searchform-wrapper" class="searchform-wrapper" action="<?php echo home_url( '/' ); ?>">
+	<div class="searchform">
 		<input type="text" value="" name="s" id="s" class="input-search" placeholder="<?php esc_html__('Buscar', 'pinplast'); ?>">
-        <div  id="activate-search">
+        <div  id="search-buttons">
             <svg width="20px" height="20px">
                 <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#search-20"></use>
             </svg>
