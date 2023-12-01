117cb027f79df422fc35f0dddd1bd58bd9cfe1a1
Chano <github@chanovera.com>
2023-11-30 22:56:34 -0600

    searchbar button | css

--- assets/js/attachments.js
+++ assets/js/attachments.js
@@ -1,44 +1,15 @@
 /* arreglo del botón del menú y del menú */
-function myFunction(x) {
-    let nav = document.getElementById("menu-mobile--wrapper");
-    let body = document.getElementById("body");
-    let header = document.getElementById("responsive-header");
-    let main = document.getElementById("main");
-    let footer = document.getElementById("main-footer");
-    x.classList.toggle("change");
-
-    if (!nav.classList.contains("active")) {
-        nav.classList.add("active");
-        body.classList.add("menu-active");
-        header.classList.add("menu-active");
-        main.classList.add("menu-active");
-        footer.classList.add("menu-active");
-    } else {
-        nav.classList.remove("active");
-        body.classList.remove("menu-active");
-        header.classList.add("menu-inactive");
-        main.classList.add("menu-inactive");
-        footer.classList.add("menu-inactive");
-        setTimeout(function() {
-            header.classList.remove("menu-inactive");
-            header.classList.remove("menu-active");
-            main.classList.remove("menu-inactive");
-            main.classList.remove("menu-active");
-            footer.classList.remove("menu-inactive");
-            footer.classList.remove("menu-active");
-        }, 300);
-    }
-}
 
 
 
+/* carga en svg4anybody después de que ha cargado todo el sitio */
 document.addEventListener('DOMContentLoaded', function() {
     svg4everybody();
 });
 
 
 
-// detecta el scroll en el sitio
+// detecta el scroll en el sitio y agrega clases según el evento desencadenado
 const body = document.body;
 const header = document.querySelector(".main-header");
 const menu = document.querySelector(".main-header .menu");
@@ -63,4 +34,19 @@ window.addEventListener("scroll", () => {
     body.classList.add(scrollUp);
   }
   lastScroll = currentScroll;
+});
+
+
+
+// muestra y oculta el cuadro de búsqueda
+let searchbarButtonOpen = document.getElementById('open-searchbar--button');
+let searchbarButtonClose = document.getElementById('close-searchbar--button');
+let searchForm = document.getElementById('searchform-wrapper');
+
+searchbarButtonOpen.addEventListener('click', function() {  
+    searchForm.classList.add('active');
+});
+
+searchbarButtonClose.addEventListener('click', function() {  
+    searchForm.classList.remove('active');
 });
