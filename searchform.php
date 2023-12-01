57e6cd1de22bd520161f2d63001e385bc03da004
Chano <github@chanovera.com>
2023-11-30 22:42:42 -0600

    searchbar button | css

--- searchform.php
+++ searchform.php
@@ -2,10 +2,10 @@
 	<div class="searchform">
 		<input type="text" value="" name="s" id="s" class="input-search" placeholder="<?php esc_html__('Buscar', 'pinplast'); ?>">
         <div  id="search-buttons">
-            <svg width="20px" height="20px">
+            <svg width="20px" height="20px" id="open-searchbar--button">
                 <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#search-20"></use>
             </svg>
-            <svg width="20px" height="20px">
+            <svg width="20px" height="20px" id="close-searchbar--button">
                 <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#cross-20"></use>
             </svg>
         </div>
