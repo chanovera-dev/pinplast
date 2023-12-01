1989d272b564fe879ed33415159d8109b1099da7
Chano <github@chanovera.com>
2023-11-30 19:15:21 -0600

    attachment list | css

--- parts/header/menu-button.php
+++ parts/header/menu-button.php
@@ -1,7 +1,5 @@
 <div class="menu-mobile--button">
-    <div class="bars" onclick="myFunction(this)">
-        <div class="bar1"></div>
-        <div class="bar2"></div>
-        <div class="bar3"></div>
-    </div>
+    <svg width="18px" height="14px">
+        <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#menu-18x14"></use>
+    </svg>
 </div>
