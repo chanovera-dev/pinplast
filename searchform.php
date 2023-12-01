83654fe69ae66672c1fbe0953cf5f6cf580dad03
Chano <github@chanovera.com>
2023-11-30 18:13:14 -0600

    searchform

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" class="searchform" action="<?php echo home_url( '/' ); ?>">
 	<div class="searchform-wrapper">
-		<input type="text" value="" name="s" id="s" class="input-search" placeholder="<?= __('Buscar', 'pinplast') ?>">
+		<input type="text" value="" name="s" id="s" class="input-search" placeholder="<?php esc_html__('Buscar', 'pinplast'); ?>">
         <div  id="activate-search">
             <svg width="20px" height="20px">
                 <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/img/sprite.svg#search-20"></use>
