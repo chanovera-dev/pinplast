e5a95d77cfdc16ee468b01a74d1849feaaefa9af
Chano <github@chanovera.com>
2023-12-02 02:32:37 -0600

    bottom bar | css | js

--- assets/js/attachments.js
+++ assets/js/attachments.js
@@ -187,12 +187,6 @@ if (window.innerWidth > 991) {
       menuLink.classList.add('hover');
   });
 
-  menuLink.addEventListener('mouseleave', function() {
-    // Agregar la clase 'open' al submenú
-    subMenu.classList.remove('open');
-    menuLink.classList.remove('hover');
-});
-
   // Agregar el evento al salir del submenú
   subMenu.addEventListener('mouseleave', function() {
       // Quitar la clase 'open' al submenú
