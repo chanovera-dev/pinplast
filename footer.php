d622459f0c0ce335e4b946ceb757cf28d82c80dc
Chano <github@chanovera.com>
2023-12-02 14:48:13 -0600

    footer | css

--- footer.php
+++ footer.php
@@ -1,14 +1,15 @@
 <?php
         echo '
         <footer id="main-footer" class="container">
-            <section class="section footer-content">
-                <aside>
+            <aside class="section footer-widgets">
+
+                <div>
                     <h2>'.esc_html__('Contacto', 'pinplast').'</h2>';
                     include(TEMPLATEPATH . '/parts/widgets/address.php');
                 echo '
-                </aside>
-                <div class="menus">';
-                    echo '
+                </div>
+
+                <div class="menus">
                     <div>';
                         $menu_id = get_nav_menu_locations()[ 'information' ];
                         $menu = wp_get_nav_menu_object( $menu_id );
@@ -38,6 +39,7 @@
                     echo '
                     </div>
                 </div>
+
                 <div class="newsletter">
                     <div class="title-wrapper">
                         <h2 class="title">Boletín de noticias</h2>
@@ -45,32 +47,31 @@
                     <p>'.esc_html__('Suscríbete a nuestros envíos de correo y recibe promociones y ofertas exclusivas.', 'pinplast').'</p>'.
                     do_shortcode('[newsletter_form]').'
                     <div>';
-                    $menu_id = get_nav_menu_locations()['social'];
-                    $menu = wp_get_nav_menu_object($menu_id);
-                    $items = wp_get_nav_menu_items($menu_id);
+                        $menu_id = get_nav_menu_locations()['social'];
+                        $menu = wp_get_nav_menu_object($menu_id);
+                        $items = wp_get_nav_menu_items($menu_id);
 
-                    if (!empty($items)) {
-                        echo '<p class="title">' . $menu->name . '</p>';
-                        wp_nav_menu(
-                            array(
-                                'container' => 'nav',
-                                'container_class' => 'social',
-                                'theme_location' => 'social',
-                            )
-                        );
-                    }
-
+                        if (!empty($items)) {
+                            echo '<p class="title">' . $menu->name . '</p>';
+                            wp_nav_menu(
+                                array(
+                                    'container' => 'nav',
+                                    'container_class' => 'social',
+                                    'theme_location' => 'social',
+                                )
+                            );
+                        }
                     echo '
                     </div>
                 </div>
-            </div>
-                <div class="copyright">'.
-                    '<p>©'.date("Y").esc_html__(' Pinplast', 'pinplast').' - '.esc_html__('Desarrollado y hospedado por ', 'pinplast').'<a href="https://peramanzana.com">PeraManzana</a></p>
-                    <div class="payments">
-                        <img src="'.get_theme_mod('payments', get_bloginfo('template_url') . '/assets/img/payments.png').'" loading="lazy">
-                    </div>
+                
+            </aside>
+            <div class="copyright">'.
+                '<p>©'.date("Y").esc_html__(' Pinplast', 'pinplast').' - '.esc_html__('Desarrollado y hospedado por ', 'pinplast').'<a href="https://peramanzana.com">PeraManzana</a></p>
+                <div class="payments">
+                    <img src="'.get_theme_mod('payments', get_bloginfo('template_url') . '/assets/img/payments.png').'" loading="lazy">
                 </div>
-            </section>
+            </div>
         </footer>';
         wp_footer();
     echo '
