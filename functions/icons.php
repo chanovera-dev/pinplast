1716f1c6d99991a99e45d87c948addc12379a646
Chano <github@chanovera.com>
2023-12-01 22:19:58 -0600

    middle bar

--- functions/icons.php
+++ functions/icons.php
@@ -2,11 +2,7 @@
 
 function pinplast_theme_custom_icons() {
     ?>
-        <style>
-            /* iconos en el menú */
-            
-            .top-bar ul li.menu-item-has-children > a:after{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/chevron-min.svg');}
-            
+        <style>          
             /* iconos de redes sociales */
             .social .menu li a[href*="facebook"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/facebook.svg');}
             .social .menu li a[href*="twitter"]:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/twitter.svg');}
---
+++ parts/header/middle-bar.php
@@ -0,0 +1,16 @@
+<?php
+    echo '
+    <div class="middle-bar-wrapper">
+        <section class="section middle-bar">';
+        include(TEMPLATEPATH . '/parts/header/brand.php');
+        get_search_form();
+        echo '
+            <div class="customer-service">';
+                echo '
+                <p>'.esc_html__('Servicio al cliente', 'pinplast').'</p>';
+                echo '
+                <a href="tel:'.get_theme_mod('customer_service', '2299216844').'"><p><span>' . get_theme_mod('customer_service', '(229) 921-6844') . '</span></p></a>';
+            echo '
+            </div>
+        </section>
+    </div>';
