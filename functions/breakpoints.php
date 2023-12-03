125be6b3e42bb8e6c2f3555154d3bc54c5ff2e26
Chano <github@chanovera.com>
2023-12-02 20:26:38 -0600

    footer | css | php

--- functions/breakpoints.php
+++ functions/breakpoints.php
@@ -39,7 +39,7 @@ function pinplast_theme_custom_breakpoints() {
                     --grid-columns--bottom-bar:21rem 1fr auto;
                     --width-attachments:4.675rem;
                     --height-attachments:4.2rem;
-                    --grid-footer-widgets:1fr 1fr 1fr;
+                    --grid-footer-widgets:1fr 1.6fr 1fr;
                     --grid-column--footer-newsletter:inherit;
                 }
                 #responsive-header{display:none;}
@@ -52,6 +52,7 @@ function pinplast_theme_custom_breakpoints() {
                     --grid-columns--middle-bar:27rem 60.34rem 1fr;
                     --grid-columns--bottom-bar:25.5rem 1fr auto;
                     --width-attachments:5.075rem;
+                    --grid-footer-widgets:1fr 1fr 1fr;
                 }
             }
         </style>
