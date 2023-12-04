42bb74dafe4d7d47e4f9c1d1032ef553b906bc44
Chano <github@chanovera.com>
2023-12-03 23:26:58 -0600

    blog, home, archive, search, etc | php | css

---
+++ home.php
@@ -0,0 +1,44 @@
+<?php
+get_header();
+
+    echo '
+    <main id="main">
+        <div class="container main-content">
+
+            <section class="section">
+                <div class="breadcrumbs">'; get_breadcrumb(); echo '</div>
+                <div class="title-wrapper">
+                    <h3>'.esc__html('Últimos artículos', 'pinplast').'</h3>
+                </div>
+            </section>
+            
+            <section class="section posts-wrapper">';
+                  
+                if ( have_posts() ){           
+                    echo '
+                    <div class="posts">';
+                        
+
+                        while( have_posts() ){            
+                            the_post();  
+                            get_template_part( 'templates/content', 'archive' );    
+                        }
+
+                        the_posts_pagination();
+                    echo '
+                    </div>';     
+                } else {
+                    echo '<p>' . esc_html__('No se encontró ninguna coincidencia', 'pinplast') . '</p>';
+                }
+
+                $post_count = wp_count_posts();
+                if ( $post_count->publish > 0 ) :
+                    include(TEMPLATEPATH . '/parts/sidebars/blog.php');
+                endif;
+
+            echo '
+            </section>
+        </div>
+    </main>';
+    
+get_footer();
