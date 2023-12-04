7e9032e9ff35125020f522c2f525c2bda13233e4
Chano <github@chanovera.com>
2023-12-04 13:18:20 -0600

    sidebar blog | php

--- functions/components.php
+++ functions/components.php
@@ -16,6 +16,7 @@ function archive_styles() {
         wp_dequeue_style( 'wp-block-library' );
         wp_enqueue_style( 'blog-styles', get_template_directory_uri() . '/assets/css/blog.css' );
         wp_enqueue_style( 'pagination-styles', get_template_directory_uri() . '/assets/css/pagination.css' );
+        wp_enqueue_style( 'widgets-styles', get_template_directory_uri() . '/assets/css/widgets.css' );
     }
 }
 add_action( 'wp_enqueue_scripts', 'archive_styles' );
