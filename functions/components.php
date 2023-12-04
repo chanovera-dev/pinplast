9134efc3a069d31e3befb6f85e5accb05914aa4e
Chano <github@chanovera.com>
2023-12-04 16:19:47 -0600

    contact blog | css | php

--- functions/components.php
+++ functions/components.php
@@ -10,6 +10,8 @@ function page404_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'page404_styles' );
 
+
+
 // Estilos para la página archivo
 function archive_styles() {
     if ( is_home() or is_page_template('home.php') or is_archive() or is_search() ) {
@@ -20,3 +22,22 @@ function archive_styles() {
     }
 }
 add_action( 'wp_enqueue_scripts', 'archive_styles' );
+
+
+
+// Estilos para el template de la página Contacto
+function contact_styles() {
+    if ( is_page_template('contact.php') ) {
+        wp_enqueue_style( 'contact-styles', get_template_directory_uri() . '/assets/css/contact.css' );
+        /* forms */
+        wp_enqueue_style( 'forms-styles', get_template_directory_uri() . '/assets/css/forms.css' );
+        // habilita contact form 7 en esta plantilla
+        if ( function_exists( 'wpcf7_enqueue_scripts' ) ) {
+            wpcf7_enqueue_scripts();
+          }
+        if ( function_exists( 'wpcf7_enqueue_styles' ) ) {
+            wpcf7_enqueue_styles();
+        }
+    }
+}
+add_action( 'wp_enqueue_scripts', 'contact_styles' );
