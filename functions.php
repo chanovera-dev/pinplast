9dc8086f1799cdfa6b066d892527b805e72c647b
Chano <github@chanovera.com>
2023-12-04 00:43:46 -0600

    blog, home, archive, search, etc | php | css

--- functions.php
+++ functions.php
@@ -92,5 +92,5 @@ function get_breadcrumb() {
 
 
 // Delimita el tamaño del excerpt a 25 palabras
-function limite_excerpt($limite) { return 25; }
+function limite_excerpt($limite) { return 19; }
 add_filter ('excerpt_length', 'limite_excerpt', 999);
