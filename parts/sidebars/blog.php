2e8f85beb6853f654e2a409cbffe586f96da4b47
Chano <github@chanovera.com>
2023-12-04 13:16:08 -0600

    sidebar blog | php

---
+++ parts/sidebars/blog.php
@@ -0,0 +1,22 @@
+<aside>
+    <div class="about-blog">
+        <h2><?php echo esc_html__('Acerca del blog', 'pinplast'); ?></h2>
+        <p><?php echo esc_html__('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt, erat in malesuada aliquam, est erat faucibus purus, eget viverra nulla sem vitae neque. Quisque id sodales libero.', 'pinplast'); ?></p>
+        <?php
+            $menu_id = get_nav_menu_locations()['social'];
+            $menu = wp_get_nav_menu_object($menu_id);
+            $items = wp_get_nav_menu_items($menu_id);
+
+            if (!empty($items)) {
+                echo '<p class="title">' . $menu->name . '</p>';
+                wp_nav_menu(
+                    array(
+                        'container' => 'nav',
+                        'container_class' => 'social',
+                        'theme_location' => 'social',
+                    )
+                );
+            }
+        ?>
+    </div>
+</aside>
