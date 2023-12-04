660e8c29f5b411f29f8bcd04c97d5683fb4d4d33
Chano <github@chanovera.com>
2023-12-04 12:51:55 -0600

    pagination | php

--- archive-product.php
+++ archive-product.php
@@ -31,7 +31,33 @@
             <section class="section">
                 <div class="woocommerce-pagination">
                     <div class="page-numbers">';
+                        global $wp_query;
+
+                        // Obtiene la página actual y el total de páginas
+                        $current_page = max(1, get_query_var('paged'));
+                        $total_pages = $wp_query->max_num_pages;
+
+                        // Muestra el botón de retroceso solo en la primera página
+                        if ($current_page === 1) {
+                            echo '
+                            <a class="prev page-numbers disabled" href="#">
+                                <svg class="page-link__arrow page-link__arrow--left" aria-hidden="true" width="8px" height="13px">
+                                    <use xlink:href="'.get_template_directory_uri().'/assets/img/sprite.svg#arrow-rounded-left-8x13"></use>
+                                </svg>
+                            </a>';
+                        }
+
                         custom_pagination();
+
+                        // Muestra el botón de avance solo en la última página
+                        if ($current_page === $total_pages) {
+                            echo '
+                            <a class="next page-numbers disabled" href="#">
+                                <svg class="page-link__arrow page-link__arrow--right" aria-hidden="true" width="8px" height="13px">
+                                    <use xlink:href="'.get_template_directory_uri().'/assets/img/sprite.svg#arrow-rounded-right-8x13"></use>
+                                </svg>
+                            </a>';
+                        }
                     echo '
                     </div>
                 </div>
