2a394d8677a93e17377b9fa68bd72169e661492d
Chano <github@chanovera.com>
2023-12-04 03:53:57 -0600

    pagination | css

--- functions.php
+++ functions.php
@@ -105,14 +105,15 @@ function custom_pagination_icons() {
             <use xlink:href="' . get_template_directory_uri() . '/assets/img/sprite.svg#arrow-rounded-left-8x13"></use>
         </svg>';
 
-    $next_icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"><path d="M6.646 8l-4-4a.5.5 0 0 1 0-.708a.5.5 0 0 1 .708 0L7.707 8l-5.354 5.354a.5.5 0 0 1-.708 0a.5.5 0 0 1 0-.708l4-4a.5.5 0 0 1 .708 0z"/></svg>';
-
+    $next_icon = '
+        <svg class="page-link__arrow page-link__arrow--right" aria-hidden="true" width="8px" height="13px">
+            <use xlink:href="' . get_template_directory_uri() . '/assets/img/sprite.svg#arrow-rounded-right-8x13"></use>
+        </svg>';
     // Obtiene los enlaces de paginación y personaliza la salida HTML
     $args = array(
         'prev_next' => true,
         'prev_text' => $previous_icon . '<span class="screen-reader-text">Previous</span>',
         'next_text' => $next_icon . '<span class="screen-reader-text">Next</span>',
-        'next_text' => true,
     );
 
     $pagination = paginate_links($args);
