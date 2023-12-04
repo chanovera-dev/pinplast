e879d85341e570fe43492d98b2db501a83bce5a4
Chano <github@chanovera.com>
2023-12-03 23:22:57 -0600

    blog, home, archive, search, etc | php | css

---
+++ archive.php
@@ -0,0 +1,45 @@
+<?php
+get_header();
+
+    echo '
+    <main id="main">
+        <div class="container main-content">
+
+            <section class="section">
+                <div class="breadcrumbs">'; get_breadcrumb(); echo '</div>
+                <div class="title-wrapper">';
+                    the_archive_title( '<h2 class="title">', '</h2>' );
+                echo '
+                </div>
+            </section>
+            
+            <section class="section posts-wrapper">';
+                  
+                if ( have_posts() ){           
+                    echo '
+                    <div class="posts">';
+                        
+
+                        while( have_posts() ){            
+                            the_post();  
+                            get_template_part( 'templates/content', 'archive' );    
+                        }
+
+                        the_posts_pagination();
+                    echo '
+                    </div>';     
+                } else {
+                    echo '<p>' . esc_html__('No se encontró ninguna coincidencia', 'pinplast') . '</p>';
+                }
+
+                $post_count = wp_count_posts();
+                if ( $post_count->publish > 0 ) :
+                    include(TEMPLATEPATH . '/parts/sidebars/blog.php');
+                endif;
+
+            echo '
+            </section>
+        </div>
+    </main>';
+    
+get_footer();
--- functions/components.php
+++ functions/components.php
@@ -9,3 +9,13 @@ function page404_styles() {
     }
 }
 add_action( 'wp_enqueue_scripts', 'page404_styles' );
+
+// Estilos para la página archivo
+function archive_styles() {
+    if ( is_home() or is_page_template('home.php') or is_archive() or is_search() ) {
+        wp_dequeue_style( 'wp-block-library' );
+        wp_enqueue_style( 'blog-styles', get_template_directory_uri() . '/assets/css/blog.css' );
+        wp_enqueue_style( 'pagination-styles', get_template_directory_uri() . '/assets/css/pagination.css' );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'archive_styles' );
