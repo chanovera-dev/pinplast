3e420368b28e4fec92f904738d42ddc677a85599
Chano <github@chanovera.com>
2023-12-05 15:37:01 -0600

    simple page | php | css

--- page.php
+++ page.php
@@ -4,11 +4,14 @@
     echo '
     <main id="main">
         <div class="container main-content">
-            <section class="section">
+            <section class="section content-header">
                 <div class="breadcrumbs">'; get_breadcrumb(); echo '</div>
+                <div class="title-wrapper">';
+                    the_title('<h1 class="title">', '</h1>');
+                echo '
+                </div>
             </section>
             <article class="section content-section border-section">';
-                the_title('<h1 class="title">', '</h1>');
                 the_content();
             echo '
             </article>
