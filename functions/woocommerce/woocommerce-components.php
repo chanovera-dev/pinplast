946c315ea943967f3d65dd77270ecf4626a3e04a
Chano <github@chanovera.com>
2023-12-05 18:18:07 -0600

    revisión por loop de woocommerce roto

--- functions/woocommerce/woocommerce-components.php
+++ functions/woocommerce/woocommerce-components.php
@@ -4,7 +4,7 @@
 function shop_styles() {
     if ( is_shop() || is_product_category() ) {
         wp_dequeue_style( 'wp-block-library' );
-        wp_enqueue_style( 'shop-styles', get_template_directory_uri() . '/assets/css/woocommerce/shop.css' );
+        
         /* listas */
         wp_enqueue_style( 'lists-styles', get_template_directory_uri() . '/assets/css/frontpage/lists.css' );
         /* estilos css para la paginación */
