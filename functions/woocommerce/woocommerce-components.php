824b8178c775cab7184e91795e514cca1e03183f
Chano <github@chanovera.com>
2023-12-05 18:01:38 -0600

    single product | php | css

--- functions/woocommerce/woocommerce-components.php
+++ functions/woocommerce/woocommerce-components.php
@@ -12,3 +12,23 @@ function shop_styles() {
     }
 }
 add_action( 'wp_enqueue_scripts', 'shop_styles' );
+
+
+
+// Estilos para la página de detalles de producto
+add_action( 'template_redirect', 'template_redirect_action' );
+function template_redirect_action() {
+    if ( ! is_admin() && is_product() ) {
+        add_filter( 'body_class', function ( $classes ) {
+            global $post;
+            $product = wc_get_product( $post->ID );
+            $tipo    = $product->get_type();
+            wp_enqueue_style( 'single-product-styles', get_template_directory_uri() . '/assets/css/woocommerce/single-product.css' );
+            // JS de ajustes
+            wp_enqueue_script( 'ajustes', get_template_directory_uri() . '/assets/js/single-product.js', '', 1, true );
+            /* estilos css para los formularios */
+            wp_enqueue_style( 'forms-styles', get_template_directory_uri() . '/assets/css/forms.css' ); 
+            return array_merge( $classes, array( $tipo ) );
+        } );
+    }
+}
