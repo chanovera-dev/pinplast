7f08ab7aa449ae280afd26020c03839994dc3715
Chano <github@chanovera.com>
2023-12-05 20:50:31 -0600

    revisión por loop de woocommerce roto

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -6,6 +6,12 @@ add_action( 'after_setup_theme', 'soporte_woocommerce' );
 
 
 
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox' );
+add_theme_support( 'wc-product-gallery-slider' );
+
+
+
 //Disable all woocommerce stylesheets
 add_filter( 'woocommerce_enqueue_styles', '__return_false' );
 
