d5fb670f6d1f8ed8310868873643544082576162
Chano <github@chanovera.com>
2023-12-05 18:15:44 -0600

    revisión por loop de woocommerce roto

--- archive-product.php
+++ archive-product.php
@@ -30,7 +30,41 @@
         <div class="container">
             <section class="section">
                 <div class="woocommerce-pagination">
-                    
+                    <div class="page-numbers">';
+                        global $wp_query;
+
+                        // Obtiene el total de páginas
+                        $total_pages = $wp_query->max_num_pages;
+                        
+                        // Verifica si hay más de una página
+                        if ($total_pages > 1) {
+                            // Obtiene la página actual
+                            $current_page = max(1, get_query_var('paged'));
+                        
+                            // Muestra el botón de retroceso solo en la primera página
+                            if ($current_page === 1) {
+                                echo '
+                                <a class="prev page-numbers disabled" href="#">
+                                    <svg class="page-link__arrow page-link__arrow--left" aria-hidden="true" width="8px" height="13px">
+                                        <use xlink:href="'.get_template_directory_uri().'/assets/img/sprite.svg#arrow-rounded-left-8x13"></use>
+                                    </svg>
+                                </a>';
+                            }
+                        
+                            custom_pagination();
+                        
+                            // Muestra el botón de avance solo en la última página
+                            if ($current_page === $total_pages) {
+                                echo '
+                                <a class="next page-numbers disabled" href="#">
+                                    <svg class="page-link__arrow page-link__arrow--right" aria-hidden="true" width="8px" height="13px">
+                                        <use xlink:href="'.get_template_directory_uri().'/assets/img/sprite.svg#arrow-rounded-right-8x13"></use>
+                                    </svg>
+                                </a>';
+                            }
+                        }
+                    echo '
+                    </div>
                 </div>
             </section>
         </div>';
