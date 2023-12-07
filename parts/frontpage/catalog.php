5230f76e6b4adfeb507ae983b1bb312669c50eea
Chano <github@chanovera.com>
2023-12-07 13:50:56 -0600

    frontpage | php | css | js

--- parts/frontpage/catalog.php
+++ parts/frontpage/catalog.php
@@ -1,6 +1,6 @@
 <section id="catalog-products" class="container main-content">
     <div class="section padding-section catalog-products-section">
-        <a class="catalog-link" href="<?php echo home_url('/'); ?>catalogo">
+        <a class="catalog-link" href="<?php echo site_url('/tienda'); ?>catalogo">
             <div class="tools">
                 <h2>Cientos</h2>
                 <p>Brochas, rodillos, bases, etcétera, etcétera ...</p>
