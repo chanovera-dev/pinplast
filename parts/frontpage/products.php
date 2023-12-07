44939b0addbe8aa79201fa3036b390fa7923f784
Chano <github@chanovera.com>
2023-12-07 13:33:18 -0600

    frontpage | php | css | js

--- parts/frontpage/products.php
+++ parts/frontpage/products.php
@@ -60,7 +60,9 @@
                         'post_status' => 'publish',
                         'posts_per_page' => 3,
                         'orderby' => 'date',
-                        'order'   => 'DESC'
+                        'order'   => 'DESC',
+                        'meta_query' => WC()->query->get_meta_query(),
+                        'post__in' => array_merge(array(0), wc_get_product_ids_on_sale())
                     );
                     $loop = new WP_Query( $args );
                     while ( $loop->have_posts() ) : $loop->the_post(); global $product;
