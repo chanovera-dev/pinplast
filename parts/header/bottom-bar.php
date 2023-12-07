a31d7a6b816f73266da301fc3510901422915fa4
Chano <github@chanovera.com>
2023-12-07 14:23:31 -0600

    BOTÓN DE COMPRAR POR CATEGORÍAS

--- parts/header/bottom-bar.php
+++ parts/header/bottom-bar.php
@@ -4,9 +4,14 @@
         <section class="section bottom-bar">';
             echo '
             <div class="">
-                <button class="departments-button">'
-                
+                <button class="departments-button">
+                    <svg width="20px" height="20px">
+                        <use xlink:href="'.get_template_directory_uri().'/assets/img/sprite.svg#cross-20"></use>
+                    </svg>'
                     .esc_html__('Comprar por categoría', 'pinplast').'
+                    <svg class="departments__button-arrow" width="9px" height="6px">
+                        <use xlink:href="'.get_template_directory_uri().'/assets/img/sprite.svg#arrow-rounded-down-9x6"></use>
+                    </svg>
                 </button>
             </div>';
             wp_nav_menu(
