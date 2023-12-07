303429b29f521021bb3ace372eeae1ba331db9fe
Chano <github@chanovera.com>
2023-12-07 10:48:37 -0600

    página carrito | js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,34 +1,36 @@
 // Encuentra todos los elementos input de tipo número dentro del carrito de WooCommerce
-const numberInputs = document.querySelectorAll('.woocommerce-cart-form input[type="number"]');
+jQuery(document).ready(function($) {
+    const numberInputs = $('.woocommerce-cart-form input[type="number"]');
 
-// Itera sobre cada input de número y agrega los botones
-numberInputs.forEach(input => {
-    const buttonLess = document.createElement("button");
-    const buttonPlus = document.createElement("button");
-    buttonLess.type = "button";
-    buttonPlus.type = "button";
-    buttonLess.innerText = "-";
-    buttonPlus.innerText = "+";
+    // Itera sobre cada input de número y agrega los botones
+    numberInputs.each(function() {
+        const buttonLess = document.createElement("button");
+        const buttonPlus = document.createElement("button");
+        buttonLess.type = "button";
+        buttonPlus.type = "button";
+        buttonLess.innerText = "-";
+        buttonPlus.innerText = "+";
 
-    function updateQuantity() {
-        const quantityInput = this.parentNode.querySelector('[type=number]');
-        const currentQuantity = parseInt(quantityInput.value, 10);
+        function updateQuantity() {
+            const quantityInput = $(this).parent().find('[type=number]');
+            const currentQuantity = parseInt(quantityInput.val(), 10);
 
-        if (this === buttonLess) {
-            quantityInput.stepDown();
-        } else if (this === buttonPlus) {
-            quantityInput.stepUp();
+            if (this === buttonLess) {
+                quantityInput[0].stepDown();
+            } else if (this === buttonPlus) {
+                quantityInput[0].stepUp();
+            }
+
+            // Actualiza la cantidad utilizando las funciones de WooCommerce
+            quantityInput.trigger('change');
         }
 
-        // Actualiza la cantidad utilizando las funciones de WooCommerce
-        $(quantityInput).trigger('change');
-    }
+        buttonLess.onclick = updateQuantity;
+        buttonPlus.onclick = updateQuantity;
 
-    buttonLess.onclick = updateQuantity;
-    buttonPlus.onclick = updateQuantity;
-
-    // Inserta los botones después del input de cantidad
-    input.insertAdjacentElement("afterend", buttonPlus);
-    // Inserta el botón de decremento antes del input de cantidad
-    input.insertAdjacentElement("beforebegin", buttonLess);
+        // Inserta los botones después del input de cantidad
+        $(this).after(buttonPlus);
+        // Inserta el botón de decremento antes del input de cantidad
+        $(this).before(buttonLess);
+    });
 });
