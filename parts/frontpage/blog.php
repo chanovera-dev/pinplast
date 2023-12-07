48358f04351f1fea8500c5f219b0e5e71f09c1f0
Chano <github@chanovera.com>
2023-12-07 14:18:20 -0600

    blog en frontpage

--- parts/frontpage/blog.php
+++ parts/frontpage/blog.php
@@ -20,7 +20,7 @@
                 <?php
                     global $post;
                     
-                    $last_posts = get_posts(array('posts_per_page' => 4));
+                    $last_posts = get_posts(array('posts_per_page' => 6));
                     
                     foreach ( $last_posts as $post ) :
                     setup_postdata( $post );
