be7032e043cedb4b937049b7c134fc7be23f3c33
Chano <github@chanovera.com>
2023-12-08 13:12:11 -0600

    hero 2

---
+++ inicio2.php
@@ -0,0 +1,10 @@
+<?php /* Template name: Inicio 2 */
+    get_header();
+
+    echo '
+    <main id="main">';
+        include(TEMPLATEPATH . '/parts/frontpage/hero2.php');
+        echo '
+    </main>';
+
+get_footer();
