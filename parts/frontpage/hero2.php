f74468745d67be282b1d573c0112d3a26d491bc7
Chano <github@chanovera.com>
2023-12-08 13:26:12 -0600

    hero 2

--- parts/frontpage/hero2.php
+++ parts/frontpage/hero2.php
@@ -4,7 +4,7 @@
         <div class="owl-carousel">
             <a class="block-slideshow__slide" href="#">
                 <div class="block-slideshow__slide-image block-slideshow__slide-image--desktop" style="background-image: url('<?php echo get_theme_mod('slide_img1', get_bloginfo('template_url') . '/assets/img/slides/slide-1.jpg'); ?>')"></div>
-                <div class="block-slideshow__slide-image block-slideshow__slide-image--mobile" style="background-image: url('images/slides/slide-1-mobile.jpg')"></div>
+                <div class="block-slideshow__slide-image block-slideshow__slide-image--mobile" style="background-image: url(''<?php echo get_theme_mod('slide_img1', get_bloginfo('template_url') . '/assets/img/slides/slide-1-mobile.jpg'); ?>')"></div>
                 <div class="block-slideshow__slide-content">
                     <div class="block-slideshow__slide-title">Big choice of<br>Plumbing products</div>
                     <div class="block-slideshow__slide-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>Etiam pharetra laoreet dui quis molestie.</div>
