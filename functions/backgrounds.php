719959f8656b58f2ce1830d50658d351d42472ec
Chano <github@chanovera.com>
2023-12-09 11:16:58 -0600

    javascript para el slidehow de hero

--- functions/backgrounds.php
+++ functions/backgrounds.php
@@ -4,6 +4,8 @@ function pinplast_theme_custom_backgrounds() {
     ?>
         <style>
             .about-us__image{background:url('<?php the_post_thumbnail_url( 'full' ); ?>'); background-repeat:no-repeat; background-position:50% 50%; background-size:cover;"}
+            .slideshow-wrapper .slideshow .slide1.mobile{background:url('<?php echo get_theme_mod('slide_img1_mobile', get_bloginfo('template_url') . '/assets/img/slides/slide-1-mobile.jpg'); ?>');}
+            .slideshow-wrapper .slideshow .slide1.desktop{background:url('<?php echo get_theme_mod('slide_img1_desktop', get_bloginfo('template_url') . '/assets/img/slides/slide-1.jpg'); ?>');}
         </style>
     <?php
 }
