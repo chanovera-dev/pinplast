1936fbe1dff193d721fde3eb618c6c29e8365866
Chano <github@chanovera.com>
2023-12-09 11:01:16 -0600

    single product · css · js

--- assets/js/attachments.js
+++ assets/js/attachments.js
@@ -258,6 +258,7 @@ if (window.innerWidth < 767) {
   let searchbarButtonOpen = document.getElementById('open-searchbar--button');
   let searchbarButtonClose = document.getElementById('close-searchbar--button');
   let searchForm = document.getElementById('searchform-wrapper');
+  let slide = document.querySelector('.slideshow-wrapper .slideshow .slide');
 
   searchbarButtonOpen.addEventListener('click', function() {  
       searchForm.classList.add('active');
@@ -266,8 +267,17 @@ if (window.innerWidth < 767) {
   searchbarButtonClose.addEventListener('click', function() {  
       searchForm.classList.remove('active');
   });
+
+  slide.classList.add('mobile');
 }
 
+
+
+if(window.innnerWidth > 768){
+  let slide = document.querySelector('.slideshow-wrapper .slideshow .slide');
+  slide.classList.remove('mobile');
+  slide.classList.add('desktop');
+}
 
 
 document.addEventListener('DOMContentLoaded', function () {
