8d741a2b49294ca9a229842bfd87ecc596a74623
Chano <github@chanovera.com>
2023-12-09 11:52:22 -0600

    php para el slidehow de hero

--- functions/backgrounds.php
+++ functions/backgrounds.php
@@ -5,21 +5,9 @@ function pinplast_theme_custom_backgrounds() {
         <style>
             .about-us__image{background:url('<?php the_post_thumbnail_url( 'full' ); ?>'); background-repeat:no-repeat; background-position:50% 50%; background-size:cover;"}
 
-            .slideshow-wrapper .slideshow .slide.mobile {
-                background: url('<?php echo get_theme_mod('slide_img1_mobile', get_bloginfo('template_url') . '/assets/img/slides/slide-1-mobile.jpg'); ?>');
-                background-repeat: no-repeat;
-                background-position: 50% 50%;
-                background-size: cover;
-            }
+            
 
-            .slideshow-wrapper .slideshow .slide.desktop {
-                background: url('<?php echo get_theme_mod('slide_img1_desktop', get_bloginfo('template_url') . '/assets/img/slides/slide-1.jpg'); ?>');
-                background-repeat: no-repeat;
-                background-position: 50% 50%;
-                background-size: cover;
-            }
-
-            #hero .hero-section .slideshow-wrapper .slideshow .slide1.desktop-full {
+            #hero .hero-section .slideshow-wrapper .slideshow .slide1 {
                 background: url('<?php echo get_theme_mod('slide_img1_desktop_full', get_bloginfo('template_url') . '/assets/img/slides/slide-1-full.jpg'); ?>');
                 background-repeat: no-repeat;
                 background-position: 50% 50%;
