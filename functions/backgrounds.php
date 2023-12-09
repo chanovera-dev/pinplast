3e752abf71a7b072c04e01ae3275b5bbad8aaf8f
Chano <github@chanovera.com>
2023-12-09 11:34:52 -0600

    php para el slidehow de hero

--- functions/backgrounds.php
+++ functions/backgrounds.php
@@ -5,8 +5,9 @@ function pinplast_theme_custom_backgrounds() {
         <style>
             .about-us__image{background:url('<?php the_post_thumbnail_url( 'full' ); ?>'); background-repeat:no-repeat; background-position:50% 50%; background-size:cover;"}
 
-            .slideshow-wrapper .slideshow .slide.slide1.mobile{background:url('<?php echo get_theme_mod('slide_img1_mobile', get_bloginfo('template_url') . '/assets/img/slides/slide-1-mobile.jpg'); ?>');}
+            .slideshow-wrapper .slideshow .slide.slide1.mobile{background:url('<?php echo get_theme_mod('slide_img1_mobile', get_bloginfo('template_url') . '/assets/img/slides/slide-1-mobile.jpg'); ?>'); background-repeat:no-repeat; background-position:50% 50%; background-size:cover;}
             .slideshow-wrapper .slideshow .slide.slide1.desktop{background:url('<?php echo get_theme_mod('slide_img1_desktop', get_bloginfo('template_url') . '/assets/img/slides/slide-1.jpg'); ?>'); background-repeat:no-repeat; background-position:50% 50%; background-size:cover;}
+            .slideshow-wrapper .slideshow .slide.slide1.desktop-full{background:url('<?php echo get_theme_mod('slide_img1_desktop_full', get_bloginfo('template_url') . '/assets/img/slides/slide-1-full.jpg'); ?>'); background-repeat:no-repeat; background-position:50% 50%; background-size:cover;}
         </style>
     <?php
 }
