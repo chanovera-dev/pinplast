15baec5df9d53a39a1a64fa5f9a7dced61715161
= <=>
2023-12-13 10:04:40 -0600

    iconos para calificar

--- functions/icons.php
+++ functions/icons.php
@@ -12,6 +12,19 @@ function pinplast_theme_custom_icons() {
 
             /* iconos de woocommerce */
             .woocommerce-product-gallery__trigger:before{background:url('<?php echo get_template_directory_uri(); ?>/assets/icons/zoom-in.svg');}
+
+            /* estrellas para calificar */
+            .woocommerce p.stars a:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star.svg');}
+            .woocommerce p.stars a.star-1:hover:before,
+            .woocommerce p.stars a.star-2:hover:before,
+            .woocommerce p.stars a.star-3:hover:before,
+            .woocommerce p.stars a.star-4:hover:before,
+            .woocommerce p.stars a.star-5:hover:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');}
+            .woocommerce p.stars a.star-1.active:before,
+            .woocommerce p.stars a.star-2.active:before,
+            .woocommerce p.stars a.star-3.active:before,
+            .woocommerce p.stars a.star-4.active:before,
+            .woocommerce p.stars a.star-5.active:before{content: ''; background-image: url('<?php echo get_template_directory_uri(); ?>/assets/icons/star-fill.svg');}
         </style>
     <?php
 }
