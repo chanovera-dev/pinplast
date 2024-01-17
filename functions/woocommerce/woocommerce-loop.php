24fe05ffdd4a249ea7116b21407edb7a8110147a
= <=>
2024-01-17 14:40:38 -0600

    ajustes a las valoraciones personalizadas

--- functions/woocommerce/woocommerce-loop.php
+++ functions/woocommerce/woocommerce-loop.php
@@ -61,6 +61,8 @@ function valoraciones_personalizadas() {
         return;
     }
 
+    $rating = wc_get_rating_html( $product->get_average_rating() );
+
     if ( $rating < 1 ) {
         echo '
         <div class="star-rating" role="img" aria-label="Valorado en ' . esc_attr( $product->get_average_rating() ) . ' de 5">
