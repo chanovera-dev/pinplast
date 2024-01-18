c27bdfeb9f1a62aef65a702078c9e60c42211edc
= <=>
2024-01-17 21:36:09 -0600

    contenedor para el carrito y la wishlist en single product para poder establecer estilos css

--- functions/woocommerce/single-product.php
+++ functions/woocommerce/single-product.php
@@ -5,7 +5,7 @@ function contenedor_antes_single_product() {
     echo '<div class="container summary"><section class="section">';
 }
 add_action('woocommerce_before_single_product', 'contenedor_antes_single_product', 12);
-// contenedor para el single product · apertura
+// contenedor para el single product · cierre
 function contenedor_despues_single_product() {
     echo '</section></div>';
 }
@@ -25,7 +25,7 @@ function contenedor_antes_pestanas_single_product() {
     echo '<div class="container tabs"><section class="section">';
 }
 add_action('woocommerce_after_single_product_summary', 'contenedor_antes_pestanas_single_product', 9);
-// contenedor para las pestañas del single product · apertura
+// contenedor para las pestañas del single product · cierre
 function contenedor_despues_pestanas_single_product() {
     echo '</section></div>';
 }
@@ -38,7 +38,7 @@ function contenedor_antes_productos_relacionados_single_product() {
     echo '<div class="container related products"><section class="section">';
 }
 add_action('woocommerce_after_single_product_summary', 'contenedor_antes_productos_relacionados_single_product', 19);
-// contenedor para los productos relacionados del single product · apertura
+// contenedor para los productos relacionados del single product · cierre
 function contenedor_despues_productos_relacionados_single_product() {
     echo '</section></div>';
 }
@@ -53,9 +53,22 @@ function woo_related_products_limit() {
     $args['posts_per_page'] = 6;
     return $args;
     }
-    add_filter( 'woocommerce_output_related_products_args', 'jk_related_products_args', 20 );
-    function jk_related_products_args( $args ) {
+    add_filter( 'woocommerce_output_related_products_args', 'pinplast_related_products_args', 20 );
+    function pinplast_related_products_args( $args ) {
     $args['posts_per_page'] = 5; 
     $args['columns'] = 1;
     return $args;
 }
+
+
+
+// contenedor para los botones de añadir carrito y añadir a la wishlist · apertura
+function contenedor_antes_carrito() {
+    echo '<div class="cart-wishlist--wrapper">';
+}
+add_action('woocommerce_single_product_summary', 'contenedor_antes_carrito', 29);
+// contenedor para los botones de añadir carrito y añadir a la wishlist · cierre
+function contenedor_despues_carrito() {
+    echo '</div>';
+}
+add_action('woocommerce_single_product_summary', 'contenedor_despues_carrito', 31);
