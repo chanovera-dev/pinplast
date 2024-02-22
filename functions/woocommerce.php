c34c49fc1f7d5e8e9090bf5a07f8a87f74808e07
Chano <136133749+chanovera-dev@users.noreply.github.com>
2024-02-22 11:58:25 -0600

    Update woocommerce.php

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -156,3 +156,18 @@ function woo_hide_page_title() { return false; }
 
 // Anexo para definir los contenedores de las listas de productos
 require_once(get_template_directory() . '/functions/woocommerce/woocommerce-loop.php');
+
+
+
+// mostrar el costo máximo en la página de productos
+// define the woocommerce_variable_price_html callback 
+function filter_woocommerce_variable_price_html( $wc_format_price_range, $instance ) { 
+    $index = strpos($wc_format_price_range, "&ndash; ");
+    $wc_format_price_range = substr($wc_format_price_range, $index+7);
+    //This will remove the first price by dividing the standard string with dash
+    //You may add a prefix like, $wc_format_price_range = "Max Price: $wc_format_price_range";
+    return $wc_format_price_range; 
+}; 
+
+// add the filter 
+add_filter( 'woocommerce_variable_price_html', 'filter_woocommerce_variable_price_html', 10, 2 );
