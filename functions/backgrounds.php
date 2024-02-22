95b50c21d2cf07e61c52bab9a25cb2b054a17aba
Chano <136133749+chanovera-dev@users.noreply.github.com>
2024-02-22 14:25:24 -0600

    Update backgrounds.php

--- functions/backgrounds.php
+++ functions/backgrounds.php
@@ -5,8 +5,8 @@ function pinplast_theme_custom_backgrounds() {
         <style>
             .page__image{background:url('<?php the_post_thumbnail_url( 'full' ); ?>'); background-repeat:no-repeat; background-position:50% 50%; background-size:cover;}
 
-            #slideshow .slide1 {background:linear-gradient(to bottom, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img1_mobile', get_bloginfo('template_url') . '/assets/img/slides/slide-1-mobile-new.jpg'); ?>');}
-            #slideshow .slide2 {background:linear-gradient(to bottom, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img2_mobile', get_bloginfo('template_url') . '/assets/img/slides/slide-2-mobile-new.jpg'); ?>');}
+            #slideshow .slide2 {background:linear-gradient(to bottom, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img1_mobile', get_bloginfo('template_url') . '/assets/img/slides/slide-1-mobile-new.jpg'); ?>');}
+            #slideshow .slide1 {background:linear-gradient(to bottom, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img2_mobile', get_bloginfo('template_url') . '/assets/img/slides/slide-2-mobile-new.jpg'); ?>');}
             #slideshow .slide3 {background:linear-gradient(to bottom, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img3_mobile', get_bloginfo('template_url') . '/assets/img/slides/slide-3-mobile-new.jpg'); ?>');}
 
             .catalog-link{background:linear-gradient(to bottom, #0000005b, #00000000), url('<?php echo get_theme_mod('catalog_link_mobile', get_bloginfo('template_url') . '/assets/img/banners/banner-1-mobile-new.jpg'); ?>');}
@@ -16,14 +16,14 @@ function pinplast_theme_custom_backgrounds() {
             }
 
             @media(min-width:1024px){
-                #slideshow .slide1{background:linear-gradient(to right, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img1_desktop', get_bloginfo('template_url') . '/assets/img/slides/slide-1-new.jpg'); ?>');}
-                #slideshow .slide2{background:linear-gradient(to right, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img2_desktop', get_bloginfo('template_url') . '/assets/img/slides/slide-2-new.jpg'); ?>');}
+                #slideshow .slide2{background:linear-gradient(to right, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img1_desktop', get_bloginfo('template_url') . '/assets/img/slides/slide-1-new.jpg'); ?>');}
+                #slideshow .slide1{background:linear-gradient(to right, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img2_desktop', get_bloginfo('template_url') . '/assets/img/slides/slide-2-new.jpg'); ?>');}
                 #slideshow .slide3{background:linear-gradient(to right, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img3_desktop', get_bloginfo('template_url') . '/assets/img/slides/slide-3-new.jpg'); ?>');}
             }
 
             @media(min-width:1200px){
-                #slideshow .slide1{background:linear-gradient(to right, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img1_desktop_full', get_bloginfo('template_url') . '/assets/img/slides/slide-1-full-new.jpg'); ?>');}
-                #slideshow .slide2{background:linear-gradient(to right, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img2_desktop_full', get_bloginfo('template_url') . '/assets/img/slides/slide-2-full-new.jpg'); ?>');}
+                #slideshow .slide2{background:linear-gradient(to right, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img1_desktop_full', get_bloginfo('template_url') . '/assets/img/slides/slide-1-full-new.jpg'); ?>');}
+                #slideshow .slide1{background:linear-gradient(to right, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img2_desktop_full', get_bloginfo('template_url') . '/assets/img/slides/slide-2-full-new.jpg'); ?>');}
                 #slideshow .slide3{background:linear-gradient(to right, #0000005b, #00000000), url('<?php echo get_theme_mod('slide_img3_desktop_full', get_bloginfo('template_url') . '/assets/img/slides/slide-3-full-new.jpg'); ?>');}
             }
         </style>
