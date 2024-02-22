796467008b90b95d3a42841d9b2991fcf899504d
Chano <136133749+chanovera-dev@users.noreply.github.com>
2024-02-22 12:01:30 -0600

    Update woocommerce.php

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -160,14 +160,14 @@ require_once(get_template_directory() . '/functions/woocommerce/woocommerce-loop
 
 
 // mostrar el costo máximo en la página de productos
-// define the woocommerce_variable_price_html callback 
-function filter_woocommerce_variable_price_html( $wc_format_price_range, $instance ) { 
-    $index = strpos($wc_format_price_range, "&ndash; ");
-    $wc_format_price_range = substr($wc_format_price_range, $index+7);
-    //This will remove the first price by dividing the standard string with dash
-    //You may add a prefix like, $wc_format_price_range = "Max Price: $wc_format_price_range";
-    return $wc_format_price_range; 
-}; 
+add_filter('woocommerce_variable_price_html', 'custom_variation_price', 10, 2);
+function custom_variation_price( $price, $product ) {
+// Get the highest priced variation
+$highest_price = $product->get_variation_price('max');
+	if ($highest_price > 0) {
+     // Display the highest price
+     $price = wc_price($highest_price);
+ }
 
-// add the filter 
-add_filter( 'woocommerce_variable_price_html', 'filter_woocommerce_variable_price_html', 10, 2 );
+ return $price;
+}
