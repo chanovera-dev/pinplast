bf72c86474c5d873a3d0ce03d199741cf918817a
Chano <136133749+chanovera-dev@users.noreply.github.com>
2024-02-22 12:17:12 -0600

    Update woocommerce.php

--- functions/woocommerce.php
+++ functions/woocommerce.php
@@ -160,28 +160,3 @@ require_once(get_template_directory() . '/functions/woocommerce/woocommerce-loop
 
 
 // mostrar el costo máximo en la página de productos
-function display_highest_price_in_loop() {
-    global $product;
-
-    if ($product->is_type('variable')) {
-        $variation_prices = array();
-
-        // Loop through variations
-        foreach ($product->get_available_variations() as $variation) {
-            $variation_prices[] = $variation['display_price'];
-        }
-
-        // Get the highest variation price
-        $highest_price = max($variation_prices);
-
-        // Display the highest price as the product price
-        $product->set_price($highest_price);
-        echo '<p>Price: ' . $product->get_price_html() . '</p>';
-    } else {
-        // For simple products or other types, just display regular price
-        echo '<p>Price: ' . $product->get_price_html() . '</p>';
-    }
-}
-
-// Hook the function into the WooCommerce product loop
-add_action('woocommerce_after_shop_loop_item', 'display_highest_price_in_loop', 10);
